881abfe38d144b9ba16993cdc3d26f079f8e2dbc
kurniawan <kurniawan9411n@gmail.com>
2023-06-11 21:53:47 +0700

    add http client, and handle login

---
+++ src/services/http_client.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import authConfig from 'src/configs/auth'
+
+axios.interceptors.request.use(
+    config => {
+        config.baseURL = "http://localhost:8080/api"
+        config.headers['Authorization'] = `Bearer ${localStorage.getItem(authConfig.storageTokenKeyName)}`;
+        return config;
+    },
+    error => {
+        return Promise.reject(error);
+    }
+);
+
+const get = (path: string) => {
+    return axios.get(path);
+}
+
+const post = (path: string, data?: any) => {
+    return axios.post(path, data);
+}
+
+export {
+    get,
+    post,
+}
