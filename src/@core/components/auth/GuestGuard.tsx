de4d23a21823c78dec39c658e7ad7df17478d7c7
kurniawan <kurniawan9411n@gmail.com>
2023-06-14 15:10:53 +0700

    fix build

--- src/@core/components/auth/GuestGuard.tsx
+++ src/@core/components/auth/GuestGuard.tsx
@@ -5,7 +5,7 @@ import { ReactNode, ReactElement, useEffect } from 'react'
 import { useRouter } from 'next/router'
 
 // ** Hooks Import
-import { useAuth } from 'src/hooks/useAuth'
+// import { useAuth } from 'src/hooks/useAuth'
 
 interface GuestGuardProps {
   children: ReactNode
@@ -13,8 +13,9 @@ interface GuestGuardProps {
 }
 
 const GuestGuard = (props: GuestGuardProps) => {
-  const { children, fallback } = props
-  const auth = useAuth();
+  const { children } = props
+
+  // const auth = useAuth();
   const router = useRouter();
 
   useEffect(() => {
