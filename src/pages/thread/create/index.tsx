a4aeb223afe0aa16695d3377a2a69024adfa5a0c
iqbal <iqbalprakasa252@gmail.com>
2023-07-16 21:17:13 +0700

    thread

---
+++ src/pages/thread/create/index.tsx
@@ -0,0 +1,131 @@
+// ** React Imports
+import React , { useEffect, useState } from 'react'
+
+// ** MUI Components
+import Box  from '@mui/material/Box'  
+import {  Autocomplete, Button,    TextField    } from '@mui/material'
+
+// import {  useTheme } from '@mui/material/styles'
+// ** Third Party Imports
+import EditorControlled from './EditorControlled'
+
+// ** Component Import
+ import {   Grid } from '@mui/material'  
+ import { EditorWrapper } from 'src/@core/styles/libs/react-draft-wysiwyg'
+import { HttpClient } from 'src/services'
+import { AppConfig } from 'src/configs/api'
+import secureLocalStorage from 'react-secure-storage'
+import localStorageKeys from 'src/configs/localstorage_keys'
+import { IUser } from 'src/contract/models/user'   
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+import Forum from 'src/contract/models/forum'
+ 
+const Thread = () => {  
+  // const theme = useTheme()  
+  const user = secureLocalStorage.getItem(localStorageKeys.userData) as IUser 
+  const [forumCode, getForumCode] = useState<[]>([])
+  const [sforumCode, setForumCode] = useState(0)
+const schema = yup.object().shape({
+  title: yup.string().min(1).required(),
+  forum: yup.string().min(1).required()
+})
+ const {
+  //  register,
+  //  formState: { errors },
+  //  handleSubmit
+ } = useForm<FormData>({
+   mode: 'onBlur',
+   resolver: yupResolver(schema)
+ })  
+ const combobox = () => {
+   HttpClient.get(AppConfig.baseUrl + '/forum?page=1&take=10&search=').then(response => {
+     const code = response.data.forums.data
+     debugger;
+     getForumCode(code)
+   })
+ }
+useEffect(() => {
+  combobox()
+}, [])
+const save = ( ) => {
+  // HttpClient.post(AppConfig.baseUrl + '/auth/register', json).then(
+  //   ({ data }) => {
+  //     console.log('here 1', data)
+  //     toast.success(data.name + ' Successfully submited!')
+  //    },
+  //   error => {
+  //     console.log('here 1', error)
+  //     toast.error('Registrastion Failed ' + error.response.data.message)
+  //   }
+  // )
+  console.log(sforumCode,user)
+}
+
+
+  return (
+    <Box>
+      <Grid container spacing={2}>
+        <Grid xs={10} container >
+          <Grid
+            item
+            container
+            xs={12}
+            sx={{
+              boxSizing: 'border-box',
+              background: '#FFFFFF',
+              border: '1px solid rgba(76, 78, 100, 0.12)',
+              borderRadius: '20px',
+              p: 4,
+              display: 'flex',
+              alignItems: 'stretch',
+              justifyContent: 'left',
+              wrap: 'nowrap'
+            }}
+          >
+            <Grid item container xs={12} spacing={2} >
+              <Grid xs={12} md={6} > 
+                <TextField
+                  id='companyName'
+                  // defaultValue={props.datauser.name}
+                  label='Company Name'
+                  variant='outlined'
+                  fullWidth
+                  sx={{ mb: 1 }} 
+                />
+              </Grid>
+              <Grid xs={12} md={6}>
+                <Autocomplete
+                  disablePortal
+                  id='code'
+                  options={forumCode}
+                  renderInput={params => <TextField {...params} label='Forum' />}
+                  getOptionLabel={(option: any) => option.name}
+                  onChange={(event: any, newValue: Forum | null) =>
+                    newValue?.id ? setForumCode(newValue.id) : setForumCode(0)
+                  }
+                />
+              </Grid>
+            </Grid>
+            <Grid xs={12}>
+              <EditorWrapper>
+                <EditorControlled />
+                <Button onClick={save}>Save</Button>
+              </EditorWrapper>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid xs={2} container></Grid>
+      </Grid>
+    </Box>
+  )
+}
+ 
+
+Thread.acl = {
+  action: 'read',
+  subject: 'home'
+};
+export default Thread
---
+++ src/pages/thread/index.tsx
@@ -0,0 +1,214 @@
+// ** React Imports
+import React , { useEffect, useState } from 'react'
+
+// ** MUI Components
+import Box  from '@mui/material/Box'  
+import {   Card, CardContent, Typography, useMediaQuery   } from '@mui/material'
+
+import {  useTheme } from '@mui/material/styles'
+// ** Layout Import
+// import BlankLayout from 'src/@core/layouts/BlankLayout'
+
+// ** Hooks 
+
+// ** Demo Imports
+// import FooterIllustrationsV2 from 'src/views/pages/auth/FooterIllustrationsV2'
+import {   Grid } from '@mui/material'  
+ 
+import Recomended from './Recomended'
+import { Icon } from '@iconify/react'
+import Profile from 'src/layouts/components/Profile'
+import Feed from 'src/layouts/components/Feed'
+import { HttpClient } from 'src/services'
+import { AppConfig } from 'src/configs/api'
+import secureLocalStorage from 'react-secure-storage'
+import localStorageKeys from 'src/configs/localstorage_keys'
+import { IUser } from 'src/contract/models/user'  
+import CommentForm from './CommentForm'
+
+const Thread = () => { 
+   const windowUrl = window.location.search
+   const params = new URLSearchParams(windowUrl)
+const theme = useTheme() 
+const hidden = useMediaQuery(theme.breakpoints.down('md')) 
+const user = secureLocalStorage.getItem(localStorageKeys.userData) as IUser
+const [userDetail, setUserDetail] = useState<IUser | null>(null)
+const [listThread, setlistThread] = useState<any>([])
+ 
+const firstload = () => {
+   debugger;
+    HttpClient.get(AppConfig.baseUrl + '/user/' + user.id).then(response => {
+      const user = response.data.user  
+      setUserDetail(user)
+    })
+  HttpClient.get(AppConfig.baseUrl + '/thread/' + params.get('id')).then(response => {
+    const user = response.data.thread  
+    setlistThread(user)
+  })
+}
+ useEffect(() => { 
+   firstload()
+ }, []) 
+ const feed = [
+  {
+    name: 'Nova Gita Taregan',
+    talent: 'Talent acquisition at barbar.com', 
+  },
+  {
+    name: 'Anis Dewinta Zahra',
+    talent: 'Talent acquisition at telcom.com', 
+  }, ]
+ 
+   
+  return (
+    <Box>
+      <Grid container spacing={2}>
+        <Grid
+          container
+          xs={12}
+          md={10}
+          sx={
+            !hidden
+              ? {
+                  alignItems: 'stretch'
+                }
+              : {}
+          }
+        >
+          <Grid item container spacing={6}>
+            <Grid item lg={4} md={5} xs={12}>
+              <Profile datauser={userDetail} />
+              <br></br>
+              <Grid container>
+                <Grid item xs={12}>
+                  <Card>
+                    <CardContent>
+                      <Box sx={{ columnGap: 2, flexWrap: 'wrap', alignItems: 'center' }} display={'flex'}>
+                        <Icon icon={'arcticons:connect-you'} fontSize={30} />{' '}
+                        <Typography variant='body1' sx={{ color: '#424242', fontWeight: 600 }}>
+                          {' '}
+                          Total Conected :250
+                        </Typography>
+                      </Box>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              </Grid>
+
+              <br></br>
+              <Feed feed={feed}></Feed>
+            </Grid>
+            <Grid item lg={8} md={7} xs={12}>
+              <Grid container spacing={6}>
+                <Grid item xs={12}>
+                  <Card>
+                    <CardContent
+                      sx={{
+                        background: 'rgba(253, 181, 40, 0.05)',
+                        border: '1px solid',
+                        borderColor: 'rgba(253, 181, 40, 0.50)'
+                      }}
+                    >
+                      <Box sx={{ mb: 7 }}>
+                        <Grid item container xs={12} justifyContent={'center'}>
+                          <Typography
+                            variant='body2'
+                            sx={{ mb: 4, color: '#424242', textTransform: 'uppercase', fontWeight: 600 }}
+                          >
+                            {listThread?.title}
+                          </Typography>
+                        </Grid>
+                        <Grid item container xs={12} justifyContent={'flex'}>
+                          <Typography
+                            variant='body1'
+                            sx={{ mb: 4, color: '#424242', textTransform: 'uppercase', fontWeight: 300 }}
+                          >
+                            {listThread?.content}
+                          </Typography>
+                        </Grid>
+                        <Grid item container xs={12} justifyContent={'flex'}>
+                          <CommentForm  />
+                           
+                        </Grid>
+                      </Box>
+                    </CardContent>
+                  </Card>
+                </Grid>
+                <Grid item xs={12}>
+                  <Recomended paramcomment={listThread}></Recomended>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid
+          xs={2}
+          container
+          display={'flex'}
+          sx={{ direction: 'row', justifyContent: 'flex-start', alignContent: 'top', alignItems: 'stretch' }}
+        >
+          <Grid xs={12}>
+            <Grid
+              xs={12}
+              sx={{
+                boxSizing: 'border-box',
+                background: '#FFFFFF',
+                border: '1px solid rgba(76, 78, 100, 0.12)',
+                borderRadius: '20px',
+                p: 4,
+                display: 'flex',
+                alignItems: 'stretch',
+                justifyContent: 'left',
+                marginBottom: '10px',
+                marginLeft: '20px',
+                height: '197px',
+                wrap: 'nowrap'
+              }}
+            ></Grid>
+            <Grid
+              xs={12}
+              sx={{
+                boxSizing: 'border-box',
+                background: '#FFFFFF',
+                border: '1px solid rgba(76, 78, 100, 0.12)',
+                borderRadius: '20px',
+                p: 4,
+                display: 'flex',
+                alignItems: 'stretch',
+                justifyContent: 'left',
+                marginBottom: '10px',
+                marginLeft: '20px',
+                height: '197px',
+                wrap: 'nowrap'
+              }}
+            ></Grid>
+            <Grid
+              xs={12}
+              sx={{
+                boxSizing: 'border-box',
+                background: '#FFFFFF',
+                border: '1px solid rgba(76, 78, 100, 0.12)',
+                borderRadius: '20px',
+                p: 4,
+                display: 'flex',
+                alignItems: 'stretch',
+                justifyContent: 'left',
+                marginBottom: '10px',
+                marginLeft: '20px',
+                height: '197px',
+                wrap: 'nowrap'
+              }}
+            ></Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Box>
+  )
+}
+ 
+
+Thread.acl = {
+  action: 'read',
+  subject: 'home'
+};
+export default Thread
