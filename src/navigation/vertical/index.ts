1a73da0cabd4e3c7cd1327ac9ff153943862e9b8
iqbal <iqbalprakasa252@gmail.com>
2023-07-26 19:23:20 +0700

    tes

--- src/navigation/vertical/index.ts
+++ src/navigation/vertical/index.ts
@@ -3,129 +3,144 @@ import { VerticalNavItemsType } from 'src/@core/layouts/types'
 
 const navigation = (): VerticalNavItemsType => {
     return [
-        //general section
-        {
-            path: '/home',
-            title: 'Home',
-            icon: 'mdi:home-outline',
-            action: 'read',
-            subject: 'home',
-        },
+      //general section
+      {
+        path: '/home',
+        title: 'Home',
+        icon: 'mdi:home-outline',
+        action: 'read',
+        subject: 'home'
+      },
 
-        // non-admin section
-        {
-            path: '/company',
-            title: 'Home',
-            icon: 'mdi:home-outline',
-            action: 'read',
-            subject: 'user-company'
-        },
-        {
-            path: '/community',
-            title: 'Community',
-            icon: 'mdi:public',
-            action: 'read',
-            subject: 'user-community'
-        },
-        {
-            path: '/job-management',
-            title: 'Manage Job',
-            icon: 'mdi:work',
-            action: 'read',
-            subject: 'user-job-management'
-        },
-        {
-            path: '/trainer/training',
-            title: 'Manage Training',
-            icon: 'mdi:work',
-            action: 'read',
-            subject: 'user-training-management'
-        },
-        {
-            path: '/company/find-candidate',
-            title: 'Find Candidate',
-            icon: 'mdi:people',
-            action: 'read',
-            subject: 'user-find-candidate'
-        },
-        {
-            path: '/trainer/my-participant',
-            title: 'List of Participant',
-            icon: 'mdi:people',
-            action: 'read',
-            subject: 'user-my-participant'
-        },
+      // non-admin section
+      {
+        path: '/candidate/find-job',
+        title: 'Find Job',
+        icon: 'mdi:work',
+        action: 'read',
+        subject: 'seaferer-jobs'
+      },
 
-        // admin section
-        {
-            path: '/admin/accounts/',
-            title: 'Accounts',
-            icon: 'mdi:user',
-            action: 'read',
-            subject: 'admin-accounts',
-        },
-        {
-            path: '/admin/job-management/',
-            title: 'Job Management',
-            icon: 'mdi:clipboard-account',
-            action: 'read',
-            subject: 'admin-job-management',
-        },
-        {
-            path: '/admin/training-management/',
-            title: 'Training Management',
-            icon: 'mdi:play',
-            action: 'read',
-            subject: 'admin-training-management',
-        },
-        {
-            path: '/admin/subcription-management/',
-            title: 'Subcription Management',
-            icon: 'mdi:cash',
-            action: 'read',
-            subject: 'admin-subcription-management',
-        },
-        {
-            path: '/admin/ads-management/',
-            title: 'Ads Management',
-            icon: 'mdi:currency-usd',
-            action: 'read',
-            subject: 'admin-ads-management',
-        },
-        {
-            path: '/admin/community-management/',
-            title: 'Community Management',
-            icon: 'mdi:account-group',
-            action: 'read',
-            subject: 'admin-community-management',
-        },
-        {
-            path: '#',
-            title: 'Master Data',
-            icon: 'mdi:email-outline',
-            action: 'read',
-            subject: 'admin-master-data',
-            children: [
-                {
-                    title: "Job Category",
-                    path: "/admin/master/job-categories",
-                    subject: "master/job-categories",
-                    action: "read",
-                },
-                {
-                    title: "Training Category",
-                    path: "/admin/master/training-categories",
-                    subject: "master/training-categories",
-                    action: "read",
-                },
-                {
-                    title: "Role Level",
-                    path: "/admin/master/role-level",
-                    subject: "master/role-level",
-                    action: "read",
-                },
-            ]
-        }
+      {
+        path: '/candidate/training',
+        title: 'My Training',
+        icon: 'mdi:play',
+        action: 'read',
+        subject: 'seaferer-training'
+      },
+      {
+        path: '/company',
+        title: 'Home',
+        icon: 'mdi:home-outline',
+        action: 'read',
+        subject: 'user-company'
+      },
+      {
+        path: '/community',
+        title: 'Community',
+        icon: 'mdi:public',
+        action: 'read',
+        subject: 'user-community'
+      },
+      {
+        path: '/job-management',
+        title: 'Manage Job',
+        icon: 'mdi:work',
+        action: 'read',
+        subject: 'user-job-management'
+      },
+      {
+        path: '/trainer/training',
+        title: 'Manage Training',
+        icon: 'mdi:work',
+        action: 'read',
+        subject: 'user-training-management'
+      },
+      {
+        path: '/company/find-candidate',
+        title: 'Find Candidate',
+        icon: 'mdi:people',
+        action: 'read',
+        subject: 'user-find-candidate'
+      },
+      {
+        path: '/trainer/my-participant',
+        title: 'List of Participant',
+        icon: 'mdi:people',
+        action: 'read',
+        subject: 'user-my-participant'
+      },
+
+      // admin section
+      {
+        path: '/admin/accounts/',
+        title: 'Accounts',
+        icon: 'mdi:user',
+        action: 'read',
+        subject: 'admin-accounts'
+      },
+      {
+        path: '/admin/job-management/',
+        title: 'Job Management',
+        icon: 'mdi:clipboard-account',
+        action: 'read',
+        subject: 'admin-job-management'
+      },
+      {
+        path: '/admin/training-management/',
+        title: 'Training Management',
+        icon: 'mdi:play',
+        action: 'read',
+        subject: 'admin-training-management'
+      },
+      {
+        path: '/admin/subcription-management/',
+        title: 'Subcription Management',
+        icon: 'mdi:cash',
+        action: 'read',
+        subject: 'admin-subcription-management'
+      },
+      {
+        path: '/admin/ads-management/',
+        title: 'Ads Management',
+        icon: 'mdi:currency-usd',
+        action: 'read',
+        subject: 'admin-ads-management'
+      },
+      {
+        path: '/admin/community-management/',
+        title: 'Community Management',
+        icon: 'mdi:account-group',
+        action: 'read',
+        subject: 'admin-community-management'
+      },
+      {
+        path: '#',
+        title: 'Master Data',
+        icon: 'mdi:email-outline',
+        action: 'read',
+        subject: 'admin-master-data',
+        children: [
+          {
+            title: 'Job Category',
+            path: '/admin/master/job-categories',
+            subject: 'master/job-categories',
+            action: 'read'
+          },
+          {
+            title: 'Training Category',
+            path: '/admin/master/training-categories',
+            subject: 'master/training-categories',
+            action: 'read'
+          },
+          {
+            title: 'Role Level',
+            path: '/admin/master/role-level',
+            subject: 'master/role-level',
+            action: 'read'
+          }
+        ]
+      }
     ]
 }
 
