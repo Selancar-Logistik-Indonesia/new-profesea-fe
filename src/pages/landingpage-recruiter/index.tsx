b957a0300b73a26d9541f7fce114b0ead6c14fd1
kurniawan <kurniawan9411n@gmail.com>
2023-07-29 15:14:48 +0700

    update landing UI

--- src/pages/landingpage-recruiter/index.tsx
+++ src/pages/landingpage-recruiter/index.tsx
@@ -1,17 +1,11 @@
-import { Button, Container, Grid, Typography, Card, CardContent } from "@mui/material";
+import { Button, Grid, Typography, Box } from "@mui/material";
 import { ReactNode } from "react";
-import LandingPageLayout from "src/@core/layouts/LandingPageLayout";
 import landingPageStyle from "src/@core/styles/landing-page/landing-page";
 import { useTranslation } from "react-i18next";
-import DiscoverView from "src/views/landing-page/discoverView";
-import FindJobsView from "src/views/landing-page/findJobsView";
 import FooterView from "src/views/landing-page/footerView";
 import Head from "next/head";
 import themeConfig from "src/configs/themeConfig";
-// ** Icon Imports
-import Icon from 'src/@core/components/icon'
-// ** Custom Components Imports
-import CustomAvatar from 'src/@core/components/mui/avatar'
+import OuterPageLayout from "src/@core/layouts/outer-components/OuterPageLayout";
 
 const Main = () => {
     const { t } = useTranslation();
@@ -26,47 +20,33 @@ const Main = () => {
             </Head>
 
             <Grid container sx={landingPageStyle.bannerHero}>
-                <Grid item xs={12} xl={6} lg={8} md={12} pt={5} mt={40} sx={{ maxWidth: { xs: '90%' }, px: { xs: 5, md: 10 } }}>
-                    <Typography variant="h4" style={{ color: "#32487A" }} fontWeight="600">{t("landing_hero_title")}</Typography>
-                    <Typography fontSize={16} style={{ color: "#424242" }} mt={8}>{t("landing_hero_subtitle")}</Typography>
-
-                    <Container style={{ marginTop: 30, lineHeight: 3.5 }}>
-                        <Grid container justifyContent="center" sx={{ backgroundColor: 'none' }} spacing={9} mt={0} mb={10}>
-                            <Card sx={{ width: 420, height: 300, backgroundColor: '#4c98cf12' }} elevation={10}>
-                                <CardContent sx={{ display: 'flex', textAlign: 'center', alignItems: 'center', flexDirection: 'column' }}>
-                                    <CustomAvatar skin='light' sx={{ width: 56, height: 56, mb: 2 }}>
-                                        <Icon icon='noto-v1:ship' fontSize='2rem' />
-                                    </CustomAvatar>
-                                    <Typography variant='h6' sx={{ mb: 2 }}>
-                                    {t('b_to_seafarer')}
-                                    </Typography>
-                                    <Typography variant='body2' sx={{ mb: 6.5, color: "#424242" }}>
-                                    {t('b_to_seafarer_detail')}
-                                    </Typography>
-                                    <Button style={{ backgroundColor: "#32487A", color: "white", marginRight: 10 }} variant="contained">{t('landing_join_now')}</Button>
-                                </CardContent>
-                            </Card>
-                            &nbsp; &nbsp;
-                            <Card sx={{ width: 420, height: 300, backgroundColor: '#4c98cf12' }} elevation={10}>
-                                <CardContent sx={{ display: 'flex', textAlign: 'center', alignItems: 'center', flexDirection: 'column' }}>
-                                    <CustomAvatar skin='light' sx={{ width: 56, height: 56, mb: 2 }}>
-                                        <Icon icon='noto:man-office-worker-light-skin-tone' fontSize='2rem' />
-                                    </CustomAvatar>
-                                    <Typography variant='h6' sx={{ mb: 2 }}>
-                                    {t('b_to_professional')}
-                                    </Typography>
-                                    <Typography variant='body2' sx={{ mb: 6.5, color: "#424242" }}>
-                                    {t('b_to_professional_detail')}
-                                    </Typography>
-                                    <Button style={{ backgroundColor: "#32487A", color: "white", marginRight: 10 }} variant="contained">{t('landing_join_now')}</Button>
-                                </CardContent>
-                            </Card>
-                        </Grid>
-                    </Container>
+                <Grid item xs={12} xl={6} lg={6} md={6} pt={5} sx={{ maxWidth: { xs: '90%' }, px: { xs: 5, md: 10 } }}>
+                    <Box sx={{ display: "flex", flexDirection: 'column', mt: 30 }}>
+                        <Box mb={4} mt={10}>
+                            <Typography variant="h4" style={{ color: "#32487A" }} fontWeight="600">{t("landing_hero_title")}</Typography>
+                            <Typography fontSize={16} style={{ color: "#424242" }}>{t("landing_hero_subtitle")}</Typography>
+                        </Box>
+                        <Box mb={4}>
+                            <Typography variant="h4" style={{ color: "#32487A" }} fontWeight="600">Lorem ipsum dolor sit amet</Typography>
+                            <Typography fontSize={16} style={{ color: "#424242" }}>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Typography>
+                        </Box>
+                        <Box>
+                            <Typography variant="h4" style={{ color: "#32487A" }} fontWeight="600">Duis aute irure dolor in reprehenderit</Typography>
+                            <Typography fontSize={16} style={{ color: "#424242" }}>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Typography>
+                        </Box>
+                    </Box>
                 </Grid>
-            </Grid>
-            <FindJobsView id="findJobSection" />
-            <DiscoverView></DiscoverView>
+                <Grid item xs={12} xl={6} lg={6} md={6} pt={5} sx={{
+                    maxWidth: { xs: '90%' }, px: { xs: 5, md: 10 },
+                    display: 'flex', flexDirection: 'column', justifyContent: 'flex-end',
+                    alignItems: "center"
+                }}>
+                    <Box mb={20}>
+                        <Button variant="outlined">Register Here</Button>
+                    </Box>
+                </Grid>
+            </Grid >
+
             <FooterView />
         </>
     );
@@ -74,6 +54,6 @@ const Main = () => {
 
 Main.guestGuard = false;
 Main.authGuard = false;
-Main.getLayout = (page: ReactNode) => <LandingPageLayout>{page}</LandingPageLayout>
+Main.getLayout = (page: ReactNode) => <OuterPageLayout>{page}</OuterPageLayout>
 
 export default Main;
