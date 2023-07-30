991365ab1d56a558d1c3173e538225d38fce252a
kurniawan <kurniawan9411n@gmail.com>
2023-07-30 11:15:37 +0700

    notification popup

---
+++ src/utils/notification_type.ts
@@ -0,0 +1,6 @@
+enum NotificationType {
+    feedLike = "App\\Notifications\\FeedLikedNotification",
+    connectRequest = "App\\Notifications\\ConnectRequestNotification",
+}
+
+export default NotificationType;
