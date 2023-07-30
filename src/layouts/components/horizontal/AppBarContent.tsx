3dfd3950766a260a60be071d2312b725e8d307be
kurniawan <kurniawan9411n@gmail.com>
2023-07-30 11:15:37 +0700

    notification popup

--- src/layouts/components/horizontal/AppBarContent.tsx
+++ src/layouts/components/horizontal/AppBarContent.tsx
@@ -3,26 +3,27 @@ import { Settings } from 'src/@core/context/settingsContext'
 import Navigation from 'src/@core/layouts/components/horizontal/navigation'
 import HorizontalNavItems from 'src/navigation/horizontal'
 import UserDropdown from 'src/@core/layouts/components/shared-components/UserDropdown'
+import NotificationDropdown, { NotificationsType } from 'src/@core/layouts/components/shared-components/NotificationDropdown'
 
 interface Props {
-  settings: Settings
-  saveSettings: (values: Settings) => void
+    settings: Settings
+    saveSettings: (values: Settings) => void
 }
 
 const AppBarContent = (props: Props) => {
-  const { settings } = props;
+    const { settings } = props;
 
-  return (
-    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-      <Navigation
-        {...props}
-        horizontalNavItems={HorizontalNavItems()}
-      />
+    return (
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Navigation
+                {...props}
+                horizontalNavItems={HorizontalNavItems()}
+            />
 
-      {/* <ModeToggler settings={settings} saveSettings={saveSettings} /> */}
-      <UserDropdown settings={settings} />
-    </Box>
-  )
+            <NotificationDropdown settings={settings} />
+            <UserDropdown settings={settings} />
+        </Box>
+    )
 }
 
 export default AppBarContent
