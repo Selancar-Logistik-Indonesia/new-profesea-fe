4fb5a93981b1e1c2ac2660c86310f4e09fc40e8f
kurniawan <kurniawan9411n@gmail.com>
2023-07-30 23:10:02 +0700

    fix build

--- src/views/landing-page/letsSailView.tsx
+++ src/views/landing-page/letsSailView.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Grid, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import Link from "next/link";
 import { useTranslation } from "react-i18next";
 
