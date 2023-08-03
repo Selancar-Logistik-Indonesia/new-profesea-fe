17dc49e4c78e8649f8d5aac405c5c2b0f7a2a20f
haisyamm <haisyammaulana22@gmail.com>
2023-08-03 23:28:19 +0700

    Filter, Job Title, Category, Role Level, Education

--- src/pages/candidate/find-job/list/index.tsx
+++ src/pages/candidate/find-job/list/index.tsx
@@ -6,8 +6,13 @@ import RecomendedView from 'src/views/find-job/RecomendedView'
 import { HttpClient } from 'src/services'
 import Job from 'src/contract/models/job'
 import InfiniteScroll from 'react-infinite-scroll-component'
+type filterType = {
+    filter: any;
+    search: string;
+    aSearch: any[];
+};
 
-const FindJob = () => {
+const FindJob = (props:filterType) => {
     const theme = useTheme()
     const hidden = useMediaQuery(theme.breakpoints.down('md'))
     const [listJob, setListJob] = useState<Job[]>([]);
@@ -18,7 +23,8 @@ const FindJob = () => {
     const [perPage, setPerPage] = useState(9);
 
     const getListJobs = async () => {
-        const response = await HttpClient.get(`/job?search=${search}&page=${page}&take=${perPage}`);
+        const response = await HttpClient.get(
+            `/job?search=${search}&roletype_id=${props?.filter?.roletype}&category_id=${props?.filter?.category}&rolelevel_id=${props?.filter?.level}&edugrade_id=${props?.filter?.education}&page=${page}&take=${perPage}`);
         const jobs = response.data.jobs.data;
         if(jobs?.total == null){
             setTotal(jobs?.total)
@@ -31,7 +37,7 @@ const FindJob = () => {
 
     useEffect(() => {
         getListJobs();
-    }, []);
+    }, [page, search, perPage, props.filter]);
 
 
     const onPageChange = () => {
