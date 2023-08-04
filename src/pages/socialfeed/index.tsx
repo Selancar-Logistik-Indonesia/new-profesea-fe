e94b81ccc3e8af41b1911500665dc6731a1b2086
kurniawan <kurniawan9411n@gmail.com>
2023-08-04 19:43:42 +0700

    update UI

--- src/pages/socialfeed/index.tsx
+++ src/pages/socialfeed/index.tsx
@@ -28,58 +28,56 @@ const SocialFeedApp = () => {
 
   return (
     <Box>
-      <Grid container spacing={2}>
-        <Grid container spacing={6}>
-          <Grid item lg={3} md={5} xs={12}>
-            <Profile datauser={user} />
-            <Grid container>
-              <Grid item my={4} xs={12}>
-                <Card>
-                  <CardContent>
-                    <Box sx={{ columnGap: 2, flexWrap: 'wrap', alignItems: 'center', mb: 2.7 }} display={'flex'}>
-                      <Icon icon={'material-symbols:tenancy'} fontSize={24} color={'#32487A'} />
-                      <Typography fontSize={12} sx={{ color: '#424242', fontWeight: 400 }}>
-                        Total Connected :250
-                      </Typography>
-                    </Box>
-                    <Box sx={{ columnGap: 2, flexWrap: 'wrap', alignItems: 'center', mb: 2.7 }} display={'flex'}>
-                      <Icon icon={'material-symbols:gesture-select-rounded'} fontSize={24} color={'#32487A'} />
-                      <Typography fontSize={12} sx={{ color: '#424242', fontWeight: 400 }}>
-                        Total Visitor :250
-                      </Typography>
-                    </Box>
-                    <Box sx={{ columnGap: 2, flexWrap: 'wrap', alignItems: 'center', mb: 2.7 }} display={'flex'}>
-                      <Icon icon={'material-symbols:add-notes-rounded'} fontSize={24} color={'#32487A'} />
-                      <Typography fontSize={12} sx={{ color: '#424242', fontWeight: 400 }}>
-                        Total Post :250
-                      </Typography>
-                    </Box>
-                    <Box sx={{ columnGap: 2, flexWrap: 'wrap', alignItems: 'center', mb: 2.7 }} display={'flex'}>
-                      <Icon icon={'material-symbols:ballot'} fontSize={24} color={'#32487A'} />
-                      <Typography fontSize={12} sx={{ color: '#424242', fontWeight: 400 }}>
-                        Total Post Job / Applied Job :250
-                      </Typography>
-                    </Box>
-                    <Box sx={{ columnGap: 2, flexWrap: 'wrap', alignItems: 'center', mb: 2.7 }} display={'flex'}>
-                      <Icon icon={'material-symbols:assignment-late'} fontSize={24} color={'#32487A'} />
-                      <Typography fontSize={12} sx={{ color: '#424242', fontWeight: 400 }}>
-                        Total Thread :250
-                      </Typography>
-                    </Box>
-                  </CardContent>
-                </Card>
-              </Grid>
-            </Grid>
-            <Feed />
-          </Grid>
-          <Grid item lg={9} md={7} xs={12}>
-            <Grid container spacing={6}>
-              <Grid item xs={12}>
-                <Postfeed />
-                <ListFeedView />
-              </Grid>
+      <Grid container spacing={6}>
+        <Grid item lg={9} md={7} xs={12}>
+          <Grid container spacing={6}>
+            <Grid item xs={12}>
+              <Postfeed />
+              <ListFeedView />
             </Grid>
           </Grid>
+        </Grid>
+        <Grid item lg={3} md={5} xs={12}>
+          <Profile datauser={user} />
+          <Grid container>
+            <Grid item my={4} xs={12}>
+              <Card>
+                <CardContent>
+                  <Box sx={{ columnGap: 2, flexWrap: 'wrap', alignItems: 'center', mb: 2.7 }} display={'flex'}>
+                    <Icon icon={'material-symbols:tenancy'} fontSize={24} color={'#32487A'} />
+                    <Typography fontSize={12} sx={{ color: '#424242', fontWeight: 400 }}>
+                      Total Connected :250
+                    </Typography>
+                  </Box>
+                  <Box sx={{ columnGap: 2, flexWrap: 'wrap', alignItems: 'center', mb: 2.7 }} display={'flex'}>
+                    <Icon icon={'material-symbols:gesture-select-rounded'} fontSize={24} color={'#32487A'} />
+                    <Typography fontSize={12} sx={{ color: '#424242', fontWeight: 400 }}>
+                      Total Visitor :250
+                    </Typography>
+                  </Box>
+                  <Box sx={{ columnGap: 2, flexWrap: 'wrap', alignItems: 'center', mb: 2.7 }} display={'flex'}>
+                    <Icon icon={'material-symbols:add-notes-rounded'} fontSize={24} color={'#32487A'} />
+                    <Typography fontSize={12} sx={{ color: '#424242', fontWeight: 400 }}>
+                      Total Post :250
+                    </Typography>
+                  </Box>
+                  <Box sx={{ columnGap: 2, flexWrap: 'wrap', alignItems: 'center', mb: 2.7 }} display={'flex'}>
+                    <Icon icon={'material-symbols:ballot'} fontSize={24} color={'#32487A'} />
+                    <Typography fontSize={12} sx={{ color: '#424242', fontWeight: 400 }}>
+                      Total Post Job / Applied Job :250
+                    </Typography>
+                  </Box>
+                  <Box sx={{ columnGap: 2, flexWrap: 'wrap', alignItems: 'center', mb: 2.7 }} display={'flex'}>
+                    <Icon icon={'material-symbols:assignment-late'} fontSize={24} color={'#32487A'} />
+                    <Typography fontSize={12} sx={{ color: '#424242', fontWeight: 400 }}>
+                      Total Thread :250
+                    </Typography>
+                  </Box>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+          <Feed />
         </Grid>
       </Grid>
     </Box>
