fc329c35eff9bf4da99dfac7c733ceb3c71aeab1
haisyamm <haisyammaulana22@gmail.com>
2023-08-07 22:53:22 +0700

    Create Thread

--- src/pages/thread/create/index.tsx
+++ src/pages/thread/create/index.tsx
@@ -3,15 +3,18 @@ import React , { useEffect, useState } from 'react'
 
 // ** MUI Components
 import Box  from '@mui/material/Box'  
-import {  Autocomplete, Button,    TextField    } from '@mui/material'
+import {  Autocomplete, Button,    CircularProgress,    TextField    } from '@mui/material'
 
 // import {  useTheme } from '@mui/material/styles'
 // ** Third Party Imports
-import EditorControlled from './EditorControlled'
 
 // ** Component Import
- import {   Grid } from '@mui/material'  
- import { EditorWrapper } from 'src/@core/styles/libs/react-draft-wysiwyg'
+import {   Grid } from '@mui/material'  
+ 
+import EditorArea from 'src/@core/components/react-draft-wysiwyg'
+import { EditorWrapper } from 'src/@core/styles/libs/react-draft-wysiwyg'
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
+
 import { HttpClient } from 'src/services'
 import { AppConfig } from 'src/configs/api'
 import secureLocalStorage from 'react-secure-storage'
@@ -21,22 +24,27 @@ import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
 import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup'
+import { EditorState, convertToRaw } from 'draft-js'
+import draftToHtml from 'draftjs-to-html';
 import Forum from 'src/contract/models/forum'
+import { toast } from 'react-hot-toast'
+import { getCleanErrorMessage } from 'src/utils/helpers'
  
 const Thread = () => {  
   // const theme = useTheme()  
+  const [onLoading, setOnLoading] = useState(false);
   const user = secureLocalStorage.getItem(localStorageKeys.userData) as IUser 
   const [forumCode, getForumCode] = useState<[]>([])
   const [sforumCode, setForumCode] = useState(0)
-const schema = yup.object().shape({
-  title: yup.string().min(1).required(),
-  forum: yup.string().min(1).required()
-})
+  const [desc, setDesc] = useState(EditorState.createEmpty())
+  const schema = yup.object().shape({
+    title: yup.string().min(1).required()
+  })
  const {
-  //  register,
-  //  formState: { errors },
-  //  handleSubmit
- } = useForm<FormData>({
+   register,
+   formState: { errors },
+   handleSubmit
+ } = useForm<any>({
    mode: 'onBlur',
    resolver: yupResolver(schema)
  })  
@@ -49,74 +57,102 @@ const schema = yup.object().shape({
 useEffect(() => {
   combobox()
 }, [])
-const save = ( ) => {
-  // HttpClient.post(AppConfig.baseUrl + '/auth/register', json).then(
-  //   ({ data }) => {
-  //     console.log('here 1', data)
-  //     toast.success(data.name + ' Successfully submited!')
-  //    },
-  //   error => {
-  //     console.log('here 1', error)
-  //     toast.error('Registrastion Failed ' + error.response.data.message)
-  //   }
-  // )
-  console.log(sforumCode,user)
-}
+const onCreate = async (formData: any) => {
+  const { title } = formData
 
+  const json = {
+      "title": title,
+      "content": draftToHtml(convertToRaw(desc?.getCurrentContent())),
+      "forum_id": sforumCode
+  }
+  setOnLoading(true);       
+  try {
+      const resp = await HttpClient.post('/thread', json);
+      if (resp.status != 200) {
+          throw resp.data.message ?? "Something went wrong!";
+      }
+
+      toast.success(` Thread created successfully!`);
+  } catch (error) {
+      toast.error(`Opps ${getCleanErrorMessage(error)}`);
+  }
+  setOnLoading(false);       
+
+}
 
   return (
     <Box>
-      <Grid container spacing={2}>
-        <Grid xs={10} container>
-          <Grid
-            item
-            container
-            xs={12}
-            sx={{
-              boxSizing: 'border-box',
-              background: '#FFFFFF',
-              border: '1px solid rgba(76, 78, 100, 0.12)',
-              borderRadius: '20px',
-              p: 4,
-              display: 'flex',
-              alignItems: 'stretch',
-              justifyContent: 'left',
-              wrap: 'nowrap'
-            }}
-          >
-            <Grid item container xs={12} columnSpacing={'1'} rowSpacing={'0,5'} sx={{ mb: 2 }}>
-              <Grid xs={12} md={6}>
-                <TextField
-                  id='companyName'
-                  // defaultValue={props.datauser.name}
-                  label='Company Name'
-                  variant='outlined'
-                  fullWidth
-                  sx={{ mb: 1 }}
-                />
+      <Grid container spacing={6}  sx={{
+        boxSizing: 'border-box',
+        background: '#FFFFFF',
+        border: '1px solid rgba(76, 78, 100, 0.12)',
+        borderRadius: '10px',
+        p: 4,
+        display: 'flex',
+        alignItems: 'stretch',
+        justifyContent: 'center',
+        wrap: 'nowrap'
+      }}>
+        <Grid item xs={10} spacing={6}>
+            <form noValidate autoComplete='off' onSubmit={handleSubmit(onCreate)} >
+              <Grid
+                item
+                container
+                xs={12}
+                sx={{
+                  boxSizing: 'border-box',
+                  background: '#FFFFFF',
+                  border: '1px solid rgba(76, 78, 100, 0.12)',
+                  borderRadius: '10px',
+                  p: 4,
+                  display: 'flex',
+                  alignItems: 'stretch',
+                  justifyContent: 'left',
+                  wrap: 'nowrap'
+                }}
+              >
+                  <Grid container xs={12} columnSpacing={'2'} rowSpacing={'2'} sx={{ mb: 2 }}>
+                    <Grid item xs={12} md={6}>
+                      <TextField
+                        id='title'
+                        {...register("title")} error={Boolean(errors.title)}
+                        label='Title'
+                        variant='outlined'
+                        fullWidth
+                        sx={{ mb: 1 }}
+                      />
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                      <Autocomplete
+                        disablePortal
+                        id='code'
+                        options={forumCode}
+                        renderInput={params => <TextField {...params} label='Forum' />}
+                        getOptionLabel={(option: any) => option.name}
+                        onChange={(event: any, newValue: Forum | null) =>
+                          newValue?.id ? setForumCode(newValue.id) : setForumCode(0)
+                        }
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <EditorWrapper>
+                          <EditorArea editorState={desc} onEditorStateChange={data => setDesc(data)} toolbar={{
+                              options: ['inline', 'blockType', 'fontSize', 'list', 'textAlign', 'history'],
+                              inline: { inDropdown: true },
+                              list: { inDropdown: true },
+                              textAlign: { inDropdown: true },
+                              link: { inDropdown: true },
+                              history: { inDropdown: true },
+                          }}  placeholder='Write a thread'/>
+                          <Button variant='contained' sx={{ mr: 2 }} type='submit'>
+                              {onLoading ? (<CircularProgress size={25} style={{ color: 'white' }} />) : "Save"}
+                          </Button>
+                      </EditorWrapper>
+                    </Grid>
+                  </Grid>
               </Grid>
-              <Grid xs={12} md={6}>
-                <Autocomplete
-                  disablePortal
-                  id='code'
-                  options={forumCode}
-                  renderInput={params => <TextField {...params} label='Forum' />}
-                  getOptionLabel={(option: any) => option.name}
-                  onChange={(event: any, newValue: Forum | null) =>
-                    newValue?.id ? setForumCode(newValue.id) : setForumCode(0)
-                  }
-                />
-              </Grid>
-            </Grid>
-            <Grid xs={12}>
-              <EditorWrapper>
-                <EditorControlled  />
-                <Button onClick={save}>Save</Button>
-              </EditorWrapper>
-            </Grid>
-          </Grid>
+            </form>
         </Grid>
-        <Grid xs={2} container></Grid>
       </Grid>
     </Box>
   )
--- src/views/community/ListThreadView.tsx
+++ src/views/community/ListThreadView.tsx
@@ -30,58 +30,60 @@ const renderList = (arr: ParamMain[]) => {
     return arr.map((item, index) => {
 
       return (
-        <Paper key={index}>
-          <Box
-            height={110}
-            sx={{
-              display: 'flex',
-              alignContent: 'center',
-              '& svg': { color: 'text.secondary' }
-            }}
-          >
-            <Grid container margin={2}>
-              <Grid xs={9}>
-                <Typography variant='body1' sx={{ color: 'text.primary', textTransform: 'uppercase' }} fontWeight={600}>
-                  {user.name}
-                </Typography>
-              </Grid>
-              <Grid container xs={3} justifyContent='flex-end'>
-                <Typography sx={{ fontWeight: 600, color: 'text.secondary' }}>
-                  {item.forum
-                    ? `${item.forum.name.toString().charAt(0).toUpperCase() + item.forum.name.toString().slice(1)}`
-                    : ''}
-                </Typography>
-              </Grid>
-              <Grid xs={12}>
-                <Typography variant='h6' sx={{ color: 'text.primary', textTransform: 'uppercase' }} fontWeight={600}>
-                  {item.title
-                    ? `${item.title.toString().charAt(0).toUpperCase() + item.title.toString().slice(1)}`
-                    : ''}
-                </Typography>
-              </Grid>
-              <Grid xs={9}>
-                <Box display={'flex'}>
-                  <Icon icon={'uil:comment'} fontSize={18} />
-                  <Typography variant='body1' sx={{ color: 'text.secondary' }} fontSize={11} fontWeight={600} marginLeft={'5px'} marginTop={'0.08rem'}>
-                    <Link href={'/thread?id=' + item.id} style={{ textDecoration: 'none' }}>
-                      Replies  {item.replies_count
-                        ? `${item.replies_count.toString().charAt(0).toUpperCase() + item.replies_count.toString().slice(1)
-                        }`
+          <Grid item xs={4} key={index}>
+            <Paper>
+              <Box
+                height={110}
+                sx={{
+                  display: 'flex',
+                  alignContent: 'center',
+                  '& svg': { color: 'text.secondary' }
+                }}
+              >
+                <Grid container margin={2}>
+                  <Grid xs={9}>
+                    <Typography variant='body1' sx={{ color: 'text.primary', textTransform: 'uppercase' }} fontWeight={600}>
+                      {user.name}
+                    </Typography>
+                  </Grid>
+                  <Grid container xs={3} justifyContent='flex-end'>
+                    <Typography sx={{ fontWeight: 600, color: 'text.secondary', fontSize:12 }}>
+                      {item.forum
+                        ? `${item.forum.name.toString().charAt(0).toUpperCase() + item.forum.name.toString().slice(1)}`
                         : ''}
-                    </Link>
+                    </Typography>
+                  </Grid>
+                  <Grid xs={12}>
+                    <Typography variant='h6' sx={{ color: 'text.primary', textTransform: 'uppercase' }} fontWeight={600}>
+                      {item.title
+                        ? `${item.title.toString().charAt(0).toUpperCase() + item.title.toString().slice(1)}`
+                        : ''}
+                    </Typography>
+                  </Grid>
+                  <Grid xs={9}>
+                    <Box display={'flex'}>
+                      <Icon icon={'uil:comment'} fontSize={18} />
+                      <Typography variant='body1' sx={{ color: 'text.secondary' }} fontSize={11} fontWeight={600} marginLeft={'5px'} marginTop={'0.08rem'}>
+                        <Link href={'/thread?id=' + item.id} style={{ textDecoration: 'none' }}>
+                          Replies  {item.replies_count
+                            ? `${item.replies_count.toString().charAt(0).toUpperCase() + item.replies_count.toString().slice(1)
+                            }`
+                            : ''}
+                        </Link>
 
-                  </Typography>
-                </Box>
-              </Grid>
-              <Grid container xs={3} justifyContent='flex-end'>
-                <Typography sx={{ fontWeight: 600, color: 'text.secondary' }}>
-                  {/* {item.created_at ? `${item.created_at.toString().charAt(0).toUpperCase() + item.crregeated_at.toString().slice(1)}` : ''} */}
-                  {Moment(item.created_at).format('DD/MM/YYYY HH:MM:SS')}
-                </Typography>
-              </Grid>
-            </Grid>
-          </Box>
-        </Paper>
+                      </Typography>
+                    </Box>
+                  </Grid>
+                  <Grid container xs={3} justifyContent='flex-end' mt={2}>
+                    <Typography sx={{ fontWeight: 600, color: 'text.secondary', fontSize:9 }}>
+                      {/* {item.created_at ? `${item.created_at.toString().charAt(0).toUpperCase() + item.crregeated_at.toString().slice(1)}` : ''} */}
+                      {Moment(item.created_at).format('DD/MM/YYYY HH:MM:SS')}
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Box>
+            </Paper>
+          </Grid>
       )
     })
   } else {
@@ -93,14 +95,26 @@ const ListThreadView = (props: Props) => {
   const { paramcomment } = props
 
   return (
-    <Grid container spacing={6}>
+    <Grid container  sx={{
+      boxSizing: 'border-box',
+      background: '#FFFFFF',
+      border: '1px solid rgba(76, 78, 100, 0.12)',
+      borderRadius: '10px',
+      p: 2,
+      display: 'flex',
+      alignItems: 'stretch',
+      justifyContent: 'center',
+      wrap: 'nowrap'
+    }}>
       <Grid item xs={12} display={'flex'} alignContent={'flex-end'} justifyContent={'flex-end'}>
         <Button variant='contained' href='/thread/create'>
           Create Thread
         </Button>
       </Grid>
       <Grid item xs={12}>
-        <Box>{renderList(paramcomment)}</Box>
+        <Grid container spacing={2}>
+          {renderList(paramcomment)}
+        </Grid>
       </Grid>
     </Grid>
   )
