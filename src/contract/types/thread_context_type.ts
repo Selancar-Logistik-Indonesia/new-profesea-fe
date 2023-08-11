2862a1d8860f6a2674e077542c7d1b6d158992e0
haisyamm <haisyammaulana22@gmail.com>
2023-08-11 08:44:27 +0700

    Fix Context

--- src/contract/types/thread_context_type.ts
+++ src/contract/types/thread_context_type.ts
@@ -1,12 +1,12 @@
-// import IThread from "../models/thread";
+import IThread from "../models/thread";
 
 type ThreadContextType = {
     page: number,
-    // totalThread: number,
+    totalThread: number,
     setPage: (page: number) => void,
-    // onLoading: boolean,
-    // threads: IThread[],
-    // hasNextPage: boolean,
+    onLoading: boolean,
+    threads: IThread[],
+    hasNextPage: boolean,
 }
 
 export default ThreadContextType;
