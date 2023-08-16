a826f30fff17b3e1bde806cf11c0c6a3979ad996
haisyamm <haisyammaulana22@gmail.com>
2023-08-16 23:26:09 +0700

    Community v2.1 Admin CRUD Community

---
+++ src/pages/admin/community-management/CommunityDatagrid.tsx
@@ -0,0 +1,84 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { DataGrid, GridCallbackDetails, GridColDef, GridPaginationModel } from '@mui/x-data-grid';
+import { IconButton } from '@mui/material';
+import Icon from 'src/@core/components/icon'
+
+const columns: GridColDef[] = [
+    { field: 'no', headerName: '#', sortable: true , minWidth: 10},
+    { field: 'title', headerName: 'Title', sortable: true , minWidth: 150},
+    { field: 'content', headerName: 'Content', sortable: true , minWidth: 300},
+    { field: 'username', headerName: 'User', sortable: false, minWidth: 200 },
+    { field: 'forum_name', headerName: 'Forum', sortable: false, minWidth: 150 },
+    {
+        field: 'action',
+        headerName: 'Action',
+        sortable: false,
+        renderCell: (cell) => {
+            const { row } = cell;
+
+            return (
+                <>
+                    <IconButton onClick={() => row.actions.onUpdate()} aria-label='edit' color='warning' size='small'>
+                        <Icon icon='mdi:pencil' />
+                    </IconButton>
+                    <IconButton onClick={() => row.actions.onDelete()} aria-label='edit' color='error' size='small'>
+                        <Icon icon='mdi:trash' />
+                    </IconButton>
+                </>
+            );
+        }
+    },
+];
+
+type RoleGridProps = {
+    rows: RowItem[];
+    loading: boolean;
+    pageSize: number;
+    page: number;
+    rowCount: number;
+    onPageChange: (model: GridPaginationModel, details: GridCallbackDetails) => void;
+}
+
+interface RowItem {
+    id:number,
+    title: string,
+    content: string,
+    username: string,
+    forum_name: string,
+    actions: {
+        onDelete: VoidFunction,
+        onUpdate: VoidFunction,
+    };
+}
+
+export {
+    type RowItem,
+}
+
+export default function AccountDatagrid(props: RoleGridProps) {
+    return (
+        <Box sx={{ height: 500, width: '100%' }}>
+            <DataGrid
+                disableColumnMenu
+                loading={props.loading}
+                rows={props.rows}
+                columns={columns}
+                paginationMode="server"
+                rowCount={props.rowCount}
+                pageSizeOptions={[10, 25, 50, 100, 250]}
+                onPaginationModelChange={props.onPageChange}
+                initialState={{
+                    pagination: {
+                        paginationModel: {
+                            pageSize: props.pageSize,
+                            page: props.page,
+                        },
+                    },
+                }}
+                disableRowSelectionOnClick
+                getRowId={(row) => row.id}
+            />
+        </Box>
+    );
+}
---
+++ src/pages/admin/community-management/edit/index.tsx
@@ -0,0 +1,193 @@
+// ** React Imports
+import React , { useEffect, useState } from 'react'
+
+// ** MUI Components
+import Box  from '@mui/material/Box'  
+import {  Autocomplete, Button,    CircularProgress,    TextField, Typography    } from '@mui/material'
+
+// import {  useTheme } from '@mui/material/styles'
+// ** Third Party Imports
+
+// ** Component Import
+import {   Grid } from '@mui/material'  
+ 
+import EditorArea from 'src/@core/components/react-draft-wysiwyg'
+import { EditorWrapper } from 'src/@core/styles/libs/react-draft-wysiwyg'
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
+
+import { HttpClient } from 'src/services'
+import { AppConfig } from 'src/configs/api'
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
+// import "../../node_modules/draft-js-image-plugin/lib/plugin.css"
+
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+import { EditorState, convertToRaw } from 'draft-js'
+import draftToHtml from 'draftjs-to-html';
+import Forum from 'src/contract/models/forum'
+import Thread from 'src/contract/models/thread'
+import { toast } from 'react-hot-toast'
+import { getCleanErrorMessage } from 'src/utils/helpers'
+ 
+const ThreadScreen = () => {  
+  // const theme = useTheme()    
+  const windowUrl = window.location.search 
+  const params = new URLSearchParams(windowUrl)
+  const [onLoading, setOnLoading] = useState(false);
+  const [forumCode, getForumCode] = useState<[]>([])
+  const [sforumCode, setForumCode] = useState(0)
+  const [threadDetail, setThreadDetail] = useState<Thread>()
+  const [desc, setDesc] = useState(EditorState.createEmpty())
+
+  const schema = yup.object().shape({
+    title: yup.string().min(1).required()
+  })
+  const {
+    register,
+    formState: { errors },
+    handleSubmit
+  } = useForm<any>({
+    mode: 'onBlur',
+    resolver: yupResolver(schema)
+  })  
+  const firstload = async () => {
+      try {
+        const resp = await HttpClient.get('/thread/' + params.get('id')) 
+        const thread  = resp.data.thread 
+        setThreadDetail(thread)
+      } catch (error) {
+        alert(error)
+      }
+
+      HttpClient.get(AppConfig.baseUrl + '/forum?page=1&take=10&search=').then(response => {
+        const code = response.data.forums.data
+        getForumCode(code)
+      })
+  }
+  useEffect(() => {
+    firstload()
+  }, [])
+
+  function uploadCallback(file:any){
+    console.log(file);
+    
+    return new Promise((resolve, reject) => {
+      const form_data = new FormData();
+      form_data.append('file', file)
+      HttpClient.postFile(`/user/filemanager` , form_data).then(response => {
+        if (response.status != 200) {
+          const error = response.data.message;
+          reject(error);
+        }
+        resolve({ data: { link: response.data.path } })
+      })
+    });
+  }
+
+  const onCreate = async (formData: any) => {
+    const { title } = formData
+
+    const json = {
+        "title": title,
+        "content": draftToHtml(convertToRaw(desc?.getCurrentContent())),
+        "forum_id": sforumCode
+    }
+    setOnLoading(true);       
+    try {
+        const resp = await HttpClient.post('/thread', json);
+        if (resp.status != 200) {
+            throw resp.data.message ?? "Something went wrong!";
+        }
+
+        toast.success(` Thread created successfully!`);
+    } catch (error) {
+        toast.error(`Opps ${getCleanErrorMessage(error)}`);
+    }
+    setOnLoading(false);       
+
+  }
+  console.log(threadDetail?.forum)
+
+  return (
+    <Box>
+      <Grid container spacing={6}  sx={{
+        boxSizing: 'border-box',
+        background: '#FFFFFF',
+        border: '1px solid rgba(76, 78, 100, 0.12)',
+        borderRadius: '10px',
+        p: 4,
+        display: 'flex',
+        alignItems: 'stretch',
+        justifyContent: 'center',
+        wrap: 'nowrap'
+      }}>
+        <Grid item xs={10} spacing={6}>
+            <form noValidate autoComplete='off' onSubmit={handleSubmit(onCreate)} >
+              <Grid
+                item
+                container
+                xs={12}
+                sx={{
+                  boxSizing: 'border-box',
+                  background: '#FFFFFF',
+                  border: '1px solid rgba(76, 78, 100, 0.12)',
+                  borderRadius: '10px',
+                  p: 4,
+                  display: 'flex',
+                  alignItems: 'stretch',
+                  justifyContent: 'left',
+                  wrap: 'nowrap'
+                }}
+              >
+                <Typography variant="body2" color={"#32487A"} fontWeight="600" fontSize={18} mb={3}> Create Thread</Typography>
+                  <Grid container xs={12} columnSpacing={'2'} rowSpacing={'2'} sx={{ mb: 2 }}>
+                    <Grid item xs={12} md={6}>
+                      <TextField
+                        id='title'
+                        {...register("title")} error={Boolean(errors.title)}
+                        value={threadDetail?.title}
+                        label='Title'
+                        variant='outlined'
+                        fullWidth
+                        sx={{ mb: 1 }}
+                      />
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                      <Autocomplete
+                        disablePortal
+                        id='code'
+                        defaultValue={threadDetail?.forum}
+                        options={forumCode}
+                        renderInput={params => <TextField {...params} label='Forum' />}
+                        getOptionLabel={(option: any) => option.name}
+                        onChange={(event: any, newValue: Forum | null) =>
+                          newValue?.id ? setForumCode(newValue.id) : setForumCode(0)
+                        }
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <EditorWrapper>
+                          <EditorArea editorState={desc} onEditorStateChange={data => setDesc(data)} toolbar={{
+                              image: { uploadCallback: uploadCallback, previewImage: true,  alt: { present: true, mandatory: false }},
+                          }}  placeholder='Write a thread' defaultContentState={threadDetail?.content}/>
+                          <Button variant='contained' sx={{ mr: 2 }} type='submit'>
+                              {onLoading ? (<CircularProgress size={25} style={{ color: 'white' }} />) : "Save"}
+                          </Button>
+                      </EditorWrapper>
+                    </Grid>
+                  </Grid>
+              </Grid>
+            </form>
+        </Grid>
+      </Grid>
+    </Box>
+  )
+}
+ 
+
+ThreadScreen.acl = {
+  action: 'read',
+  subject: 'admin-community-management'
+};
+export default ThreadScreen
