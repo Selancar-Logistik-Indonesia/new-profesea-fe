a58efe7fbd37b54bf0c2c1d9c3b96eb015026240
haisyamm <haisyammaulana22@gmail.com>
2023-08-25 14:20:20 +0700

    Instant Training

---
+++ src/contract/models/question.ts
@@ -0,0 +1,11 @@
+
+interface Question {
+    training_id: number
+    form_type: string
+    question: string
+    choices:any
+    created_at: string
+    updated_at: string
+}
+
+export default Question;
