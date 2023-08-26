feae770aa4054703e00b4b8765f1f380da0919e3
kurniawan <kurniawan9411n@gmail.com>
2023-08-26 09:02:13 +0700

    pay-per-value

--- src/views/payment/PaymentSubscriptionDialog.tsx
+++ src/views/payment/PaymentSubscriptionDialog.tsx
@@ -1,16 +1,19 @@
 import { Icon } from "@iconify/react";
-import { Box, Button, Card, CircularProgress, Dialog, DialogActions, DialogContent, Divider, FormControl, IconButton, InputLabel, List, ListItem, ListItemAvatar, ListItemSecondaryAction, ListItemText, MenuItem, Radio, Select, Typography } from "@mui/material";
+import { Box, Button, Card, Checkbox, CircularProgress, Dialog, DialogActions, DialogContent, Divider, FormControl, IconButton, InputLabel, List, ListItem, ListItemAvatar, ListItemSecondaryAction, ListItemText, MenuItem, Radio, Select, Typography } from "@mui/material";
+import collect from "collect.js";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import IBank from "src/contract/models/bank";
+import Pricing from "src/contract/models/pricing";
 import { HttpClient } from "src/services";
-import { formatIDR } from "src/utils/helpers";
+import { formatIDR, getCleanErrorMessage } from "src/utils/helpers";
 
 type SelectedPlan = {
     planName: string,
     price: number,
     planType: string,
+    pricingItems?: Pricing[],
 }
 
 type Props = {
@@ -26,6 +29,23 @@ const PaymentSubscriptionDialog = (props: Props) => {
     const [selectedBank, setSelectedBank] = useState('');
     const [selectedDuration, setSelectedDuration] = useState<number>(1);
     const router = useRouter();
+    const { selectedPlan } = props;
+    const [packages, setPackages] = useState<Pricing[]>([]);
+
+    const handleChangePackage = (item: Pricing) => {
+        setPackages((old) => {
+            let items = old.filter(e => e.id != item.id);
+            const exists = old.includes(item);
+            if (!exists) {
+                items = [
+                    ...items,
+                    item
+                ];
+            }
+
+            return items;
+        });
+    }
 
     const getListBanks = async () => {
         setOnLoading(['widget']);
@@ -48,32 +68,48 @@ const PaymentSubscriptionDialog = (props: Props) => {
         }
 
         setOnLoading(['checkout']);
-        const response = await HttpClient.post('/transaction/create', {
-            "payment_type": "VA",
-            "bank_code": selectedBank,
-            "trxable_ids": [props.selectedPlan.planType],
-            "trxable_type": props.trxAbleType,
-            "qty": selectedDuration,
-        });
+        try {
+            let payload = null;
+            if (selectedPlan.planType != "pay-per-value") {
+                payload = {
+                    "payment_type": "VA",
+                    "bank_code": selectedBank,
+                    "trxable_ids": [selectedPlan.planType],
+                    "trxable_type": props.trxAbleType,
+                    "qty": selectedDuration,
+                };
+            } else {
+                payload = {
+                    "payment_type": "VA",
+                    "bank_code": selectedBank,
+                    "trxable_ids": packages.map(e => e.id),
+                    "trxable_type": selectedPlan.planType,
+                    "qty": selectedDuration,
+                };
+            }
 
-        if (response.status != 200) {
-            alert(response.data?.message ?? "Internal server error!");
+            const response = await HttpClient.post('/transaction/create', payload);
+            if (response.status != 200) {
+                throw response.data?.message ?? "Internal server error!";
+            }
 
-            return;
+            router.push(`/transaction/detail/${response.data.transaction.trx_id}`);
+        } catch (error) {
+            toast.error(getCleanErrorMessage(error));
         }
 
         setOnLoading([]);
-        router.push(`/transaction/detail/${response.data.transaction.trx_id}`);
     }
 
     useEffect(() => {
         getListBanks();
     }, []);
 
+    console.log(selectedPlan);
     return (
         <Dialog fullWidth={true} open={props.openDialog} onClose={() => props.onClose()}>
             <DialogActions sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-                <Typography variant="body1" fontSize={24}>Transaction</Typography>
+                <Typography variant="body1" fontSize={24}>Subscription</Typography>
                 <IconButton onClick={() => props.onClose()}>
                     <Icon icon='mdi:close' />
                 </IconButton>
@@ -83,13 +119,35 @@ const PaymentSubscriptionDialog = (props: Props) => {
                     <Card>
                         <ListItem>
                             <ListItemText>
-                                <Typography variant="body1" fontSize={24}>{props.selectedPlan.planName}</Typography>
+                                <Typography variant="body1" fontSize={24}>{selectedPlan.planName}</Typography>
                             </ListItemText>
                         </ListItem>
 
+                        {selectedPlan.planType == "pay-per-value" && (
+                            <Box sx={{ mx: 5, mb: 7 }}>
+                                <Typography variant="body1" mt={10} ml={5} mb={3} fontSize={14}>Pilih Fitur</Typography>
+                                <Divider />
+                                <List>
+                                    {selectedPlan.pricingItems!.map(e => (
+                                        <ListItem key={e.id} sx={{ cursor: 'pointer' }} onClick={() => handleChangePackage(e)}>
+                                            <ListItemAvatar>
+                                                <Checkbox checked={packages.includes(e)} radioGroup="pay-per-value" value={e} />
+                                            </ListItemAvatar>
+                                            <ListItemText>
+                                                <Box>
+                                                    <Typography variant="body1">{e.item.name}</Typography>
+                                                    <Typography variant="caption">{formatIDR(e.price)}</Typography>
+                                                </Box>
+                                            </ListItemText>
+                                        </ListItem>
+                                    ))}
+                                </List>
+                            </Box>
+                        )}
+
                         <Box sx={{ mx: 5, mt: 3 }}>
                             <FormControl fullWidth>
-                                <InputLabel sx={{ fontSize: 16 }} id="demo-simple-select-label">Subscription Duration</InputLabel>
+                                <InputLabel sx={{ fontSize: 16 }} id="demo-simple-select-label">Durasi berlangganan</InputLabel>
                                 <Select
                                     variant="standard"
                                     labelId="demo-simple-select-label"
@@ -107,7 +165,15 @@ const PaymentSubscriptionDialog = (props: Props) => {
 
                         <Box mx={10} my={5}>
                             <Typography variant="caption">Jumlah yang harus dibayar:</Typography>
-                            <Typography variant="body1" fontSize={24}>{formatIDR((props.selectedPlan.price * selectedDuration))}</Typography>
+                            {
+                                (selectedPlan.planType == "pay-per-value") ? (
+                                    <Typography variant="body1" fontSize={24}>{formatIDR(
+                                        parseFloat(collect(packages.map(e => e.price * selectedDuration)).sum().toString())
+                                    )}</Typography>
+                                ) : (
+                                    <Typography variant="body1" fontSize={24}>{formatIDR((selectedPlan.price * selectedDuration))}</Typography>
+                                )
+                            }
                         </Box>
                     </Card>
                 </List>
