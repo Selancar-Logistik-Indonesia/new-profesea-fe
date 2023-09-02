f9d138dc6c460a01b0a79020fb4b998b1f3e349a
haisyamm <haisyammaulana22@gmail.com>
2023-09-02 23:17:51 +0700

    Dashboard top list

--- src/contract/types/dashboard_context_type.ts
+++ src/contract/types/dashboard_context_type.ts
@@ -3,9 +3,13 @@ type DashboardContextType = {
     dataOnship: any[],
     dataOffship: any[],
     dataTotalUser: any[],
+    dataTopCompany: any[],
+    dataTopCommunity: any[],
+    dataTopTraining: any[],
     statOfCandidateOff: () => Promise<void>,
     statOfCandidateOn: () => Promise<void>,
     statOfUserByRole: () => Promise<void>,
+    statTopList: (payload: {contribType: string, segment?:any}) => Promise<void>,
 }
 
 export default DashboardContextType;
--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -5,9 +5,9 @@ import Grid from '@mui/material/Grid'
 // import CompanyGraph from 'src/views/admin/dashboard/analytics/CompanyGraph'
 // import SubscriberGraph from 'src/views/admin/dashboard/analytics/SubscriberGraph'
 // import CandidateGraph from 'src/views/admin/dashboard/analytics/CandidateGraph'
-// import TopPostCompany from 'src/views/admin/dashboard/analytics/TopPostCompany'
-// import TopCommunityContributor from 'src/views/admin/dashboard/analytics/TopCommunityContributor'
-// import TopTrainingAttendance from 'src/views/admin/dashboard/analytics/TopTrainingAttendance'
+import TopPostCompany from 'src/views/admin/dashboard/analytics/TopPostCompany'
+import TopCommunityContributor from 'src/views/admin/dashboard/analytics/TopCommunityContributor'
+import TopTrainingAttendance from 'src/views/admin/dashboard/analytics/TopTrainingAttendance'
 import CardStatOnship from 'src/views/admin/dashboard/CardStatOnship'
 import CardStatOffship from 'src/views/admin/dashboard/CardStatOffship'
 import CardStatByRole from 'src/views/admin/dashboard/CardStatUserByRole'
@@ -28,7 +28,7 @@ const Home = () => {
         <SubscriberGraph />
       </Grid> */}
       <CardStatByRole></CardStatByRole>
-      {/* <Grid item xs={12} sm={6} md={4}>
+      <Grid item xs={12} sm={6} md={4}>
         <TopPostCompany />
       </Grid>
       <Grid item xs={12} sm={6} md={4}>
@@ -36,7 +36,7 @@ const Home = () => {
       </Grid>
       <Grid item xs={12} sm={6} md={4}>
         <TopTrainingAttendance />
-      </Grid> */}
+      </Grid>
     </Grid>
   )
 }
