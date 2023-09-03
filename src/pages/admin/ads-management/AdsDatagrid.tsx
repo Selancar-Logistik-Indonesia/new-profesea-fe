48cae39828c89bd98eb00c9bba70a1a40875df0a
haisyamm <haisyammaulana22@gmail.com>
2023-09-03 20:24:30 +0700

    Ads Management

---
+++ src/pages/admin/ads-management/AdsDatagrid.tsx
@@ -0,0 +1,85 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { DataGrid, GridCallbackDetails, GridColDef, GridPaginationModel } from '@mui/x-data-grid';
+import { IconButton } from '@mui/material';
+import Icon from 'src/@core/components/icon'
+
+const columns: GridColDef[] = [
+    { field: 'no', headerName: '#', sortable: true , width: 50},
+    { field: 'image', headerName: 'Location Storage', sortable: false, minWidth: 600 },
+    { field: 'expired_at', headerName: 'Expired At', sortable: false, minWidth: 150 },
+    {
+        field: 'action',
+        headerName: 'Action',
+        sortable: false,
+        minWidth: 150,
+        renderCell: (cell) => {
+            const { row } = cell;
+
+            return (
+                <>
+                    <IconButton onClick={() => row.actions.onView()} aria-label='view' color='secondary' size='small'>
+                        <Icon icon='solar:eye-scan-bold-duotone' />
+                    </IconButton>
+                    <IconButton onClick={() => row.actions.onUpdate()} aria-label='edit' color='warning' size='small'>
+                        <Icon icon='solar:pen-new-round-bold-duotone' />
+                    </IconButton>
+                    <IconButton onClick={() => row.actions.onDelete()} aria-label='edit' color='error' size='small'>
+                        <Icon icon='solar:trash-bin-trash-bold-duotone' />
+                    </IconButton>
+                </>
+            );
+        }
+    },
+];
+
+type RoleGridProps = {
+    rows: RowItem[];
+    loading: boolean;
+    pageSize: number;
+    page: number;
+    rowCount: number;
+    onPageChange: (model: GridPaginationModel, details: GridCallbackDetails) => void;
+}
+
+interface RowItem {
+    id:number,
+    expired_at: string,
+    image: string,
+    actions: {
+        onDelete: VoidFunction,
+        onUpdate: VoidFunction,
+        onView: VoidFunction,
+    };
+}
+
+export {
+    type RowItem,
+}
+
+export default function AccountDatagrid(props: RoleGridProps) {
+    return (
+        <Box sx={{ height: 500, width: '100%' }}>
+            <DataGrid
+                disableColumnMenu
+                loading={props.loading}
+                rows={props.rows}
+                columns={columns}
+                paginationMode="server"
+                rowCount={props.rowCount}
+                pageSizeOptions={[10, 25, 50, 100, 250]}
+                onPaginationModelChange={props.onPageChange}
+                initialState={{
+                    pagination: {
+                        paginationModel: {
+                            pageSize: props.pageSize,
+                            page: props.page,
+                        },
+                    },
+                }}
+                disableRowSelectionOnClick
+                getRowId={(row) => row.id}
+            />
+        </Box>
+    );
+}
---
+++ src/pages/admin/ads-management/DialogAdd.tsx
@@ -0,0 +1,239 @@
+import { Ref, forwardRef, ReactElement, useState } from 'react'
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import Dialog from '@mui/material/Dialog'
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import Fade, { FadeProps } from '@mui/material/Fade'
+import DialogContent from '@mui/material/DialogContent'
+import DialogActions from '@mui/material/DialogActions'
+import { styled } from '@mui/material/styles'
+import toast from 'react-hot-toast'
+import Icon from 'src/@core/components/icon'
+import { useForm } from 'react-hook-form'
+import { HttpClient } from 'src/services'
+import { getCleanErrorMessage } from 'src/utils/helpers'
+import { CircularProgress } from '@mui/material'
+// import User from 'src/contract/models/company'
+import { DateType } from 'src/contract/models/DatepickerTypes'
+// import { Autocomplete } from '@mui/material'
+import DatePicker from 'react-datepicker'
+import DatePickerWrapper from 'src/@core/styles/libs/react-datepicker'
+import { useDropzone } from 'react-dropzone'
+
+
+import Link from 'next/link'
+
+const Transition = forwardRef(function Transition(
+    props: FadeProps & { children?: ReactElement<any, any> },
+    ref: Ref<unknown>
+) {
+    return <Fade ref={ref} {...props} />
+})
+
+type DialogProps = {
+    visible: boolean;
+    onCloseClick: VoidFunction;
+    onStateChange: VoidFunction;
+}
+
+interface FileProp {
+    name: string
+    type: string
+    size: number
+}
+
+const Img = styled('img')(({ theme }) => ({
+    [theme.breakpoints.up('md')]: {
+      marginRight: theme.spacing(10)
+    },
+    [theme.breakpoints.down('md')]: {
+      marginBottom: theme.spacing(4)
+    },
+    [theme.breakpoints.down('sm')]: {
+      width: 250
+    }
+}))
+
+const DialogAdd = (props: DialogProps) => {
+    const [onLoading, setOnLoading] = useState(false);
+    // const [UserId, setUserId] = useState(0);
+    const [date, setDate] = useState<DateType>(new Date())
+    const [files, setFiles] = useState<File[]>([])    
+    // const [User, getUser] =useState<any[]>([])
+
+    const { getRootProps, getInputProps } = useDropzone({
+        multiple: false,
+        accept: {
+        'image/*': ['.png', '.jpg', '.jpeg', '.gif']
+        },
+        onDrop: (acceptedFiles: File[]) => {
+        setFiles(acceptedFiles.map((file: File) => Object.assign(file)))
+        }
+    })
+
+    const img = files.map((file: FileProp) => (
+        <img key={file.name} alt={file.name} className='single-file-image' src={URL.createObjectURL(file as any)} width={450} />
+    ))
+    
+    // const combobox = async () =>{
+        
+    //     const resp = await HttpClient.get(`/user-management?page=1&take=250&team_id=4`);
+    //     if (resp.status != 200) {
+    //         throw resp.data.message ?? "Something went wrong!";
+    //     }
+    //     getUser(resp.data.users.data);
+
+    // }
+
+    // useEffect(() => {   
+    // combobox()
+    // },[]) 
+
+    const { 
+        handleSubmit,
+    } = useForm<any[]>({
+        mode: 'onBlur'
+    }) 
+
+
+    const onSubmit = async () => {
+        
+        const json = {
+            // "user_id": UserId,
+            "attachments": files,
+            "expired_at": date?.toLocaleDateString("en-GB", {
+                year: "numeric",
+                month: "2-digit",
+                day: "2-digit"
+            }).split('/').reverse().join('-')+" "
+            +date?.toTimeString().split(' ')[0]
+        }
+        
+        setOnLoading(true);
+
+        try
+        {
+            const resp = await HttpClient.postFile('/ads', json);
+            if (resp.status != 200) {
+                throw resp.data.message ?? "Something went wrong!";
+            }
+
+            props.onCloseClick();
+            toast.success(` Ads submited successfully!`);
+        } catch (error) {
+            toast.error(`Opps ${getCleanErrorMessage(error)}`);
+        }
+
+        setOnLoading(false);
+        props.onStateChange();
+    }
+
+    return (
+        <Dialog
+            fullWidth
+            open={props.visible}
+            maxWidth='sm'
+            scroll='body'
+            TransitionComponent={Transition}
+        >
+            <form noValidate autoComplete='off' onSubmit={handleSubmit(onSubmit)}  >
+                <DialogContent
+                    sx={{
+                        position: 'relative',
+                        pb: theme => `${theme.spacing(8)} !important`,
+                        px: theme => [`${theme.spacing(5)} !important`, `${theme.spacing(15)} !important`],
+                        pt: theme => [`${theme.spacing(8)} !important`, `${theme.spacing(12.5)} !important`]
+                    }}
+                >
+                    <IconButton
+                        size='small'
+                        sx={{ position: 'absolute', right: '1rem', top: '1rem' }}
+                        onClick={props.onCloseClick}
+                    >
+                        <Icon icon='mdi:close' />
+                    </IconButton>
+                    <Box sx={{ mb: 6, textAlign: 'center' }}>
+                    <Typography variant="body2" color={"#32487A"} fontWeight="600" fontSize={18}>
+                            Add New ADS
+                        </Typography>
+                        <Typography variant='body2'>Fulfill your ADS Info here</Typography>
+                    </Box>
+                    
+                    <Grid container columnSpacing={'1'} rowSpacing={'4'} >
+                        {/* <Grid item md={12} xs={12} > 
+                            <Autocomplete
+                                disablePortal
+                                id="combo-box-Trainer"
+                                options={User}  
+                                getOptionLabel={(option:Trainer) => option.name}
+                                renderInput={(params) => <TextField {...params} label="Trainer" error={Boolean(errors.user_id)} {...register("user_id")}/>}
+                                onChange={(event: any, newValue: Trainer | null)=> (newValue?.id) ? setUserId(newValue.id) : setUserId(0)}
+                            />
+                        </Grid> */}
+                        <Grid item md={12} xs={12} >
+                            <DatePickerWrapper>
+                                <DatePicker
+                                showTimeSelect
+                                minDate={new Date()}
+                                dateFormat='dd/MM/yyyy hh:mm aa'
+                                selected={date}
+                                id='basic-input'
+                                onChange={(date: Date) => setDate(date)}
+                                placeholderText='Click to select a date'
+                                customInput={<TextField label='Expired At' variant="outlined" fullWidth />}
+                                />
+                            </DatePickerWrapper>
+                        </Grid> 
+                        <Grid item md={12} xs={12} >
+                        <Box  {...getRootProps({ className: 'dropzone' })} sx={{ p: 2, border: '1px dashed' }}>
+                            <input {...getInputProps()} />
+                            {files.length ? (
+                                img
+                            ) : (
+                                <Box sx={{ display: 'flex', flexDirection: ['column', 'column', 'row'], alignItems: 'center' }}>
+                                <Img width={200} alt='Upload img' src='/images/upload.png' />
+                                <Box sx={{ display: 'flex', flexDirection: 'column', textAlign: ['center', 'center', 'inherit'] }}>
+                                    <Typography variant='h5'  color='textSecondary' sx={{ '& a': { color: 'primary.main', textDecoration: 'none' } }}>
+                                    Click{' '}
+                                    <Link href='/' onClick={e => e.preventDefault()}>
+                                        browse / image
+                                    </Link>{' '}
+                                    to upload ADS
+                                    </Typography>
+                                </Box>
+                                </Box>
+                            )}
+                            </Box>
+                        </Grid>                      
+                    </Grid>
+                </DialogContent>
+                <DialogActions
+                    sx={{
+                        justifyContent: 'center',
+                        px: theme => [`${theme.spacing(5)} !important`, `${theme.spacing(15)} !important`],
+                        pb: theme => [`${theme.spacing(8)} !important`, `${theme.spacing(12.5)} !important`]
+                    }}
+                >
+                    <Button variant='contained' size="small" sx={{ mr: 2 }} type='submit'>
+                    <Icon fontSize='large' icon={'solar:diskette-bold-duotone'} color={'info'} style={{ fontSize: '18px' }} />
+                        {onLoading ? (<CircularProgress size={25} style={{ color: 'white' }} />) : "Submit"}
+                    </Button>
+                    <Button variant='outlined' size="small" color='error' onClick={props.onCloseClick}>
+                    <Icon
+                            fontSize='large'
+                            icon={'material-symbols:cancel-outline'}
+                            color={'info'}
+                            style={{ fontSize: '18px' }}
+                        />
+                        Cancel
+                    </Button>
+                </DialogActions>
+            </form>
+        </Dialog>
+    )
+}
+
+export default DialogAdd
