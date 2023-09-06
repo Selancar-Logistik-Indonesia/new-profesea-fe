ed8ddd4c164b013ae50201b4313622d98e6d24e0
haisyamm <haisyammaulana22@gmail.com>
2023-09-06 09:23:34 +0700

    Fix Build

--- src/pages/candidate/training/in-house/ButtonJoin.tsx
+++ src/pages/candidate/training/in-house/ButtonJoin.tsx
@@ -1,15 +1,7 @@
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import { Box, Button, CircularProgress } from '@mui/material';
-import { useEffect } from 'react';
-import Training from 'src/contract/models/training';
-import Icon from 'src/@core/components/icon';
-import Link from 'next/link';
-import TrainingContext, { TrainingProvider } from 'src/context/TrainingContext';
+
+import { Button } from '@mui/material';
+import TrainingContext from 'src/context/TrainingContext';
 import { useTraining } from 'src/hooks/useTraining';
-import InfiniteScroll from 'react-infinite-scroll-component';
 
 const ButtonJoin = (props : {id:number}) => {
     const { joinTraining } = useTraining();
