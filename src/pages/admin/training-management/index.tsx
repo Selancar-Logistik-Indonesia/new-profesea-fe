416565498243154601e2fffef63a44e0ba678401
haisyamm <haisyammaulana22@gmail.com>
2023-09-08 05:51:46 +0700

    Improvement Filter

--- src/pages/admin/training-management/index.tsx
+++ src/pages/admin/training-management/index.tsx
@@ -3,7 +3,7 @@ import CardHeader from '@mui/material/CardHeader'
 import CardContent from '@mui/material/CardContent'
 import Grid from '@mui/material/Grid'
 import TextField from '@mui/material/TextField';
-import { Box, Button, Typography } from '@mui/material'
+import { Autocomplete, Box, Button, Typography } from '@mui/material'
 import { useCallback, useEffect, useState } from 'react'
 import DialogAdd from './DialogAdd';
 import TrainingDatagrid, { RowItem } from './Trainingtagrid';
@@ -18,6 +18,10 @@ import DialogEdit from './DialogEdit';
 import { v4 } from "uuid";
 import DialogView from './DialogView';
 import { Icon } from '@iconify/react';
+import { DateType } from 'src/contract/models/DatepickerTypes';
+import DatePicker from 'react-datepicker'
+import DatePickerWrapper from 'src/@core/styles/libs/react-datepicker'
+import TrainingCategory from 'src/contract/models/training_category';
 
 const TrainingScreen = () => {
     const [hookSignature, setHookSignature] = useState(v4())
@@ -29,6 +33,9 @@ const TrainingScreen = () => {
     const [dataSheet, setDataSheet] = useState<RowItem[]>([]);
     const [selectedItem, setSelectedItem] = useState<Training | null>(null);
 
+    const [CatId, setCatId] = useState(0);
+    const [filterDate, setDate] = useState<DateType>(new Date())
+    const [TrainingCategory, getTrainingCategory] =useState<any[]>([]);
     const [page, setPage] = useState(1);
     const [rowCount, setRowCount] = useState(0);
     const [search, setSearch] = useState("");
@@ -36,7 +43,7 @@ const TrainingScreen = () => {
     const [perPage, setPerPage] = useState(10);
     const getListTraining = async () => {
         try {
-            const resp = await HttpClient.get(`/training?search=${search}&page=${page}&take=${perPage}`);
+            const resp = await HttpClient.get(`/training?search=${search}&page=${page}&take=${perPage}&category_id=${CatId}`);
             if (resp.status != 200) {
                 throw resp.data.message ?? "Something went wrong!";
             }
@@ -74,6 +81,13 @@ const TrainingScreen = () => {
             toast.error(`Opps ${errorMessage}`);
         }
     }
+    const combobox = async () =>{
+        const res = await HttpClient.get(`/training-category?search=&page=1&take=250`);
+        if (res.status != 200) {
+            throw res.data.message ?? "Something went wrong!";
+        }
+        getTrainingCategory(res.data.trainingCategories.data);
+    }
 
     const handleSearch = useCallback(
         debounce((value: string) => {
@@ -107,7 +121,8 @@ const TrainingScreen = () => {
         getListTraining().then(() => {
             setOnLoading(false);
         });
-    }, [page, search, hookSignature, perPage]);
+        combobox()
+    }, [page, search, hookSignature, perPage, CatId, filterDate]);
 
     return (
         <>
@@ -122,6 +137,33 @@ const TrainingScreen = () => {
                           }
                         />
                         <CardContent>
+                            <Grid container justifyContent="flex-start">
+                                <Grid item>
+                                    <Autocomplete
+                                        disablePortal
+                                        size='small'
+                                        sx={{ mb: 2, width: '150px', mr:2 }}
+                                        id="combo-box-demo"
+                                        options={TrainingCategory}  getOptionLabel={(option:TrainingCategory) => option.category}
+                                        renderInput={(params) => <TextField {...params} label="Training Category" />}
+                                        onChange={(event: any, newValue: TrainingCategory | null)=> (newValue?.id) ? setCatId(newValue.id) : setCatId(0)}
+                                        />
+                                </Grid>
+                                <Grid item>
+                                    <DatePickerWrapper>
+                                        <DatePicker
+                                        showTimeSelect
+                                        minDate={new Date()}
+                                        dateFormat='dd/MM/yyyy hh:mm aa'
+                                        selected={filterDate}
+                                        id='basic-input'
+                                        onChange={(date: Date) => setDate(date)}
+                                        placeholderText='Click to select a date'
+                                        customInput={<TextField size='small' label='Schedule' variant="outlined" fullWidth />}
+                                        />
+                                    </DatePickerWrapper>
+                                </Grid>
+                            </Grid>
                             <Grid container justifyContent="flex-end">
                                 <Grid item>
                                     <TextField
