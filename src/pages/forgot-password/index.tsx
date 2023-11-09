c61bd274d99a80ea875e5598a0eb90a99f934bf5
haisyamm <haisyammaulana22@gmail.com>
2023-11-09 23:49:00 +0700

    Forgot & Reset Password

--- src/pages/forgot-password/index.tsx
+++ src/pages/forgot-password/index.tsx
@@ -1,5 +1,6 @@
 // ** React Imports
-import { ReactNode } from 'react'
+import { ReactNode, useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 // ** Next Import
 import Link from 'next/link'
@@ -11,6 +12,11 @@ import Box, { BoxProps } from '@mui/material/Box'
 import useMediaQuery from '@mui/material/useMediaQuery'
 import { styled, useTheme } from '@mui/material/styles'
 import Typography, { TypographyProps } from '@mui/material/Typography'
+import toast from 'react-hot-toast'
+import { getCleanErrorMessage } from 'src/utils/helpers'
+import { HttpClient } from 'src/services'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
 
 // ** Icon Imports
 import Icon from 'src/@core/components/icon'
@@ -25,26 +31,9 @@ import BlankLayout from 'src/@core/layouts/BlankLayout'
 import { useSettings } from 'src/@core/hooks/useSettings'
 
 // ** Demo Imports
-import FooterIllustrationsV2 from 'src/views/pages/auth/FooterIllustrationsV2'
+import Head from 'next/head'
 
-// Styled Components
-const ForgotPasswordIllustrationWrapper = styled(Box)<BoxProps>(({ theme }) => ({
-  padding: theme.spacing(20),
-  paddingRight: '0 !important',
-  [theme.breakpoints.down('lg')]: {
-    padding: theme.spacing(10)
-  }
-}))
 
-const ForgotPasswordIllustration = styled('img')(({ theme }) => ({
-  maxWidth: '48rem',
-  [theme.breakpoints.down('xl')]: {
-    maxWidth: '38rem'
-  },
-  [theme.breakpoints.down('lg')]: {
-    maxWidth: '30rem'
-  }
-}))
 
 const RightWrapper = styled(Box)<BoxProps>(({ theme }) => ({
   width: '100%',
@@ -79,149 +68,120 @@ const LinkStyled = styled(Link)(({ theme }) => ({
   color: theme.palette.primary.main
 }))
 
+type FormData = {
+  email: string
+}
+
 const ForgotPassword = () => {
   // ** Hooks
   const theme = useTheme()
   const { settings } = useSettings()
+  const [message, setMessage]  = useState("")
+  const schema = yup.object().shape({
+    email: yup.string().email().required()
+  })
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+      mode: 'onBlur',
+      resolver: yupResolver(schema)
+  })
+
+  const onSubmit = async (data : FormData) => {
+    const { email } = data;
+
+      const json = {
+          "email": email
+      };
+
+      try {
+          const response = await HttpClient.post('/auth/forgot-password', json);
+          // if (response.status != 200) {
+              setMessage(response.data?.message)
+          // }
+
+      } catch (error) {
+          toast.error(`Opps ${getCleanErrorMessage(error)}`);
+      }
+  }
 
   // ** Vars
   const { skin } = settings
   const hidden = useMediaQuery(theme.breakpoints.down('md'))
 
-  const imageSource =
-    skin === 'bordered' ? 'auth-v2-forgot-password-illustration-bordered' : 'auth-v2-forgot-password-illustration'
-
   return (
-    <Box className='content-right'>
-      {!hidden ? (
-        <Box sx={{ flex: 1, display: 'flex', position: 'relative', alignItems: 'center', justifyContent: 'center' }}>
-          <ForgotPasswordIllustrationWrapper>
-            <ForgotPasswordIllustration
-              alt='forgot-password-illustration'
-              src={`/images/pages/${imageSource}-${theme.palette.mode}.png`}
-            />
-          </ForgotPasswordIllustrationWrapper>
-          <FooterIllustrationsV2 image={`/images/pages/auth-v2-forgot-password-mask-${theme.palette.mode}.png`} />
-        </Box>
-      ) : null}
-      <RightWrapper sx={skin === 'bordered' && !hidden ? { borderLeft: `1px solid ${theme.palette.divider}` } : {}}>
-        <Box
-          sx={{
-            p: 7,
-            height: '100%',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: 'background.paper'
-          }}
-        >
-          <BoxWrapper>
-            <Box
-              sx={{
-                top: 30,
-                left: 40,
-                display: 'flex',
-                position: 'absolute',
-                alignItems: 'center',
-                justifyContent: 'center'
-              }}
-            >
-              <svg width={47} fill='none' height={26} viewBox='0 0 268 150' xmlns='http://www.w3.org/2000/svg'>
-                <rect
-                  rx='25.1443'
-                  width='50.2886'
-                  height='143.953'
-                  fill={theme.palette.primary.main}
-                  transform='matrix(-0.865206 0.501417 0.498585 0.866841 195.571 0)'
-                />
-                <rect
-                  rx='25.1443'
-                  width='50.2886'
-                  height='143.953'
-                  fillOpacity='0.4'
-                  fill='url(#paint0_linear_7821_79167)'
-                  transform='matrix(-0.865206 0.501417 0.498585 0.866841 196.084 0)'
-                />
-                <rect
-                  rx='25.1443'
-                  width='50.2886'
-                  height='143.953'
-                  fill={theme.palette.primary.main}
-                  transform='matrix(0.865206 0.501417 -0.498585 0.866841 173.147 0)'
-                />
-                <rect
-                  rx='25.1443'
-                  width='50.2886'
-                  height='143.953'
-                  fill={theme.palette.primary.main}
-                  transform='matrix(-0.865206 0.501417 0.498585 0.866841 94.1973 0)'
-                />
-                <rect
-                  rx='25.1443'
-                  width='50.2886'
-                  height='143.953'
-                  fillOpacity='0.4'
-                  fill='url(#paint1_linear_7821_79167)'
-                  transform='matrix(-0.865206 0.501417 0.498585 0.866841 94.1973 0)'
-                />
-                <rect
-                  rx='25.1443'
-                  width='50.2886'
-                  height='143.953'
-                  fill={theme.palette.primary.main}
-                  transform='matrix(0.865206 0.501417 -0.498585 0.866841 71.7728 0)'
-                />
-                <defs>
-                  <linearGradient
-                    y1='0'
-                    x1='25.1443'
-                    x2='25.1443'
-                    y2='143.953'
-                    id='paint0_linear_7821_79167'
-                    gradientUnits='userSpaceOnUse'
+    <>
+    <Head>
+      <title>{`${themeConfig.templateName} - Forgot Password Page`}</title>
+    </Head>
+    <Box sx={{
+      position: 'fit',
+      width: '100%',
+      height: '100%',
+      backgroundImage: "url(/images/bglogin.jpg)",
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+    }}>
+      <Box className='content-right'>
+        <RightWrapper sx={skin === 'bordered' && !hidden ? { borderLeft: `1px solid ${theme.palette.divider}` } : {}}>
+          <Box
+            sx={{
+              p: 7,
+              height: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: 'background.paper'
+            }}
+          >
+            <BoxWrapper>
+              <Box sx={{ mb: 10, maxWidth: '100%', justifyContent: 'center', alignContent: 'center', textAlign: 'center' }}>
+                <Link href='/'>
+                  <Box
+                    component="img"
+                    src='/images/logosamudera.png'
+                    sx={{ width: 225, mt: 5 }}
                   >
-                    <stop />
-                    <stop offset='1' stopOpacity='0' />
-                  </linearGradient>
-                  <linearGradient
-                    y1='0'
-                    x1='25.1443'
-                    x2='25.1443'
-                    y2='143.953'
-                    id='paint1_linear_7821_79167'
-                    gradientUnits='userSpaceOnUse'
-                  >
-                    <stop />
-                    <stop offset='1' stopOpacity='0' />
-                  </linearGradient>
-                </defs>
-              </svg>
-              <Typography variant='h6' sx={{ ml: 2, lineHeight: 1, fontWeight: 700, fontSize: '1.5rem !important' }}>
-                {themeConfig.templateName}
-              </Typography>
-            </Box>
-            <Box sx={{ mb: 6 }}>
-              <TypographyStyled variant='h5'>Forgot Password? 🔒</TypographyStyled>
-              <Typography variant='body2'>
-                Enter your email and we&prime;ll send you instructions to reset your password
-              </Typography>
-            </Box>
-            <form noValidate autoComplete='off' onSubmit={e => e.preventDefault()}>
-              <TextField autoFocus type='email' label='Email' sx={{ display: 'flex', mb: 4 }} />
-              <Button fullWidth size='large' type='submit' variant='contained' sx={{ mb: 5.25 }}>
-                Send reset link
-              </Button>
-              <Typography sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                <LinkStyled href='/login'>
-                  <Icon icon='mdi:chevron-left' fontSize='2rem' />
-                  <span>Back to login</span>
-                </LinkStyled>
-              </Typography>
-            </form>
-          </BoxWrapper>
-        </Box>
-      </RightWrapper>
+                  </Box>
+                </Link>
+              </Box>
+              {(message == "") ? (
+                <>
+                <Box sx={{ mb: 6 }}>
+                  <TypographyStyled variant='h5'>Forgot Password? 🔒</TypographyStyled>
+                  <Typography variant='body2'>
+                    Enter your email and we&prime;ll send you instructions to reset your password
+                  </Typography>
+                </Box>
+                <form noValidate autoComplete='off' onSubmit={handleSubmit(onSubmit)}>
+                  <TextField autoFocus type='email' label='Email' sx={{ display: 'flex', mb: 4 }} error={Boolean(errors.email)} {...register("email")} />
+                  <Button fullWidth size='large' type='submit' variant='contained' sx={{ mb: 5.25 }}>
+                    Send reset link
+                  </Button>
+                  <Typography sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    <LinkStyled href='/login'>
+                      <Icon icon='mdi:chevron-left' fontSize='2rem' />
+                      <span>Back to login</span>
+                    </LinkStyled>
+                  </Typography>
+                </form>
+                </>
+              ) : (
+                <>
+                <Box sx={{ mb: 6 }}>
+                  <TypographyStyled variant='h5'> We have e-mailed your password reset link!</TypographyStyled>
+                </Box>
+                </>
+              )}
+            </BoxWrapper>
+          </Box>
+        </RightWrapper>
+      </Box>
     </Box>
+    </>
   )
 }
 
