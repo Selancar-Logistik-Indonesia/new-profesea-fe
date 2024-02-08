28e2e6f49885414267c26378c1cbb33dcaaba579
sudaishakim <dais@inspigo.id>
2024-02-08 17:02:43 +0700

    move component ads to the left at landing page

--- src/pages/socialfeed/index.tsx
+++ src/pages/socialfeed/index.tsx
@@ -61,79 +61,7 @@ const SocialFeedApp = () => {
       <Grid container spacing={6}>
         <Grid item lg={3} md={5} xs={12}>
           <Profile datauser={user} />
-          {/* <Grid container>
-            <Grid item my={4} xs={12}>
-              <Card sx={{ border: 0, boxShadow: 0, color: 'common.white', backgroundColor: '#FFFFFF' }}>
-                <CardContent>
-                  <Typography color={'#262525'} fontWeight='800' fontSize={'14px'} sx={{ mb: 4 }}>
-                    Who See Your Profile
-                  </Typography>
-                  <Box sx={{ columnGap: 2, flexWrap: 'wrap', alignItems: 'center', mb: 2.7 }} display={'flex'}>
-                    <Link href={show}>
-                      <AvatarGroup className='pull-up' max={4}>
-                        <Avatar src='/images/avatars/avatar-3.png' alt='1' />
-                        <Avatar src='/images/avatars/avatar-4.png' alt='2' />
-                        <Avatar src='/images/avatars/avatar-5.png' alt='3' />
-                        <Avatar src='/images/avatars/avatar-6.png' alt='4' />
-                        <Avatar src='/images/avatars/avatar-7.png' alt='5' />
-                        <Avatar src='/images/avatars/avatar-8.png' alt='6' />
-                      </AvatarGroup>
-                    </Link>
-                  </Box>
-                </CardContent>
-                <CardContent>
-                  <Typography color={'#262525'} fontWeight='800' fontSize={'14px'} sx={{ mb: 4 }}>
-                    Your Activities
-                  </Typography>
-                  <Box sx={{ columnGap: 2, flexWrap: 'wrap', alignItems: 'center', mb: 2.7 }} display={'flex'}>
-                    <Icon icon={'solar:user-speak-rounded-bold-duotone'} fontSize={20} color={'#262525'} />
-                    <Typography fontSize={12} sx={{ color: '#262525', fontWeight: 400 }}>
-                      Total Connected : {activities?.total_connected}
-                    </Typography>
-                  </Box>
-                  <Box sx={{ columnGap: 2, flexWrap: 'wrap', alignItems: 'center', mb: 2.7 }} display={'flex'}>
-                    <Icon icon={'solar:to-pip-bold-duotone'} fontSize={20} color={'#262525'} />
-                    <Typography fontSize={12} sx={{ color: '#262525', fontWeight: 400 }}>
-                      Total Visitor :{activities?.total_visitor}
-                    </Typography>
-                  </Box>
-                  {user?.role == 'Company' ? (
-                    <>
-                      <Box sx={{ columnGap: 2, flexWrap: 'wrap', alignItems: 'center', mb: 2.7 }} display={'flex'}>
-                        <Icon icon={'solar:case-minimalistic-bold-duotone'} fontSize={20} color={'#262525'} />
-                        <Typography fontSize={12} sx={{ color: '#262525', fontWeight: 400 }}>
-                          Total Post Job :{activities?.total_post_feed}
-                        </Typography>
-                      </Box>
-                    </>
-                  ) : (
-                    <Box sx={{ columnGap: 2, flexWrap: 'wrap', alignItems: 'center', mb: 2.7 }} display={'flex'}>
-                      <Icon icon={'solar:case-minimalistic-bold-duotone'} fontSize={20} color={'#262525'} />
-                      <Typography fontSize={12} sx={{ color: '#262525', fontWeight: 400 }}>
-                        Applied Job : {activities?.total_applied_job}
-                      </Typography>
-                    </Box>
-                  )}
-                  <Box sx={{ columnGap: 2, flexWrap: 'wrap', alignItems: 'center', mb: 2.7 }} display={'flex'}>
-                    <Icon icon={'solar:clapperboard-text-bold-duotone'} fontSize={20} color={'#262525'} />
-                    <Typography fontSize={12} sx={{ color: '#262525', fontWeight: 400 }}>
-                      Total Post :{activities?.total_visitor}
-                    </Typography>
-                  </Box>
-                  <Box sx={{ columnGap: 2, flexWrap: 'wrap', alignItems: 'center', mb: 2.7 }} display={'flex'}>
-                    <Icon icon={'solar:plate-bold-duotone'} fontSize={20} color={'#262525'} />
-                    <Typography fontSize={12} sx={{ color: '#262525', fontWeight: 400 }}>
-                      Total Thread : {activities?.total_post_thread}
-                    </Typography>
-                  </Box>
-                </CardContent>
-              </Card>
-            </Grid>
-          </Grid> */}
-          <Box my={4}>
-            <FriendSuggestionCard />
-          </Box>
-          {/* <Divider sx={{ my: '0 !important' }} /> */}
+
           <KeenSliderWrapper>
             <SideAd />
           </KeenSliderWrapper>
@@ -212,6 +140,9 @@ const SocialFeedApp = () => {
               </Box>
             </CardContent>
           </Card>
+          <Box my={4}>
+            <FriendSuggestionCard />
+          </Box>
         </Grid>
       </Grid>
     </Box>
