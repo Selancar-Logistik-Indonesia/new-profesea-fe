49aad562c2ecfc512fa37b9a628582038e9174ea
Dais <dais@Daiss-MacBook-Air.local>
2024-02-16 05:44:30 +0700

    update job card layout and content

--- src/views/find-job/RecomendedView.tsx
+++ src/views/find-job/RecomendedView.tsx
@@ -5,8 +5,9 @@ import Icon from 'src/@core/components/icon'
 import { Avatar, Paper } from '@mui/material'
 import Job from 'src/contract/models/job'
 import Link from 'next/link'
+import { format } from 'date-fns'
+import moment from 'moment'
 
- 
 export type ParamMain = {
   name: string
   skill: string
@@ -22,34 +23,49 @@ const renderList = (listJob: Job[]) => {
     return <></>
   }
 
+  console.log(listJob)
 
-  console.log(listJob);
-
-  return listJob.map((item) => {
-    const userPhoto = (item?.company?.photo) ? item?.company?.photo : "/images/avatars/default-user.png";
+  return listJob.map(item => {
+    const userPhoto = item?.company?.photo ? item?.company?.photo : '/images/avatars/default-user.png'
     const license: any[] = Object.values(item?.license)
- 
+
     return (
       <Grid item xs={12} md={4} key={item?.id}>
-        <Paper sx={{ marginTop: '10px', border: '1px solid #eee', height: 185 }} elevation={0}>
+        <Paper sx={{ marginTop: '10px', border: '1px solid #eee', height: '250px' }} elevation={0}>
           <Link style={{ textDecoration: 'none' }} href={'/candidate/job/?id=' + item?.id}>
             <Box
               height={65}
               sx={{
                 display: 'flex',
                 alignContent: 'center',
-                '& svg': { color: 'text.secondary' }
+                justifyContent: 'space-between'
               }}
             >
-              <Box sx={{ display: 'flex', justifyContent: 'center' }} mt={2} ml={2} mr={3}>
-                <Avatar src={userPhoto} alt='profile-picture' sx={{ width: 50, height: 50 }} />
+              <Box
+                sx={{
+                  display: 'flex',
+                  alignContent: 'center',
+                  '& svg': { color: 'text.secondary' }
+                }}
+              >
+                <Box sx={{ display: 'flex', justifyContent: 'center' }} mt={2} ml={2} mr={3}>
+                  <Avatar src={userPhoto} alt='profile-picture' sx={{ width: 50, height: 50 }} />
+                </Box>
+                <Box
+                  sx={{ display: 'flex', flexDirection: 'column', alignItems: ['left', 'flex-start'] }}
+                  marginTop={2}
+                >
+                  <Typography sx={{ fontWeight: 'bold', color: '#0a66c2', mb: 1 }} fontSize={14}>
+                    {item?.role_type?.name ?? '-'}
+                  </Typography>
+                  <Typography sx={{ color: 'text.primary', mb: 1 }} fontSize={12}>
+                    {item?.company?.name ?? '-'}
+                  </Typography>
+                </Box>
               </Box>
-              <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: ['left', 'flex-start'] }} marginTop={2}>
-                <Typography sx={{ fontWeight: 'bold', color: '#0a66c2', mb: 1 }} fontSize={14}>
-                  {item?.role_type?.name ?? '-'}
-                </Typography>
-                <Typography sx={{ color: 'text.primary', mb: 1 }} fontSize={12}>
-                  {item?.company?.name ?? '-'}
+              <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                <Typography sx={{ color: 'text.primary', mt: 2, mr: 2 }} fontSize={12}>
+                  {item?.created_at ? moment(item.created_at).fromNow() : '-'}
                 </Typography>
               </Box>
             </Box>
@@ -94,6 +110,36 @@ const renderList = (listJob: Job[]) => {
                     {item?.vessel_type?.name}
                   </Typography>
                 </Grid>
+                <Grid xs={1}>
+                  <Icon icon='ri:calendar-fill' color='#32487A' fontSize={'20px'} />
+                </Grid>
+                <Grid xs={11} maxWidth={'90%'}>
+                  <Typography sx={{ color: 'text.primary' }} ml='0.5rem' mt='0.2rem' fontSize={12}>
+                    {format(new Date(item?.onboard_at), 'dd MMMM yyyy')}
+                  </Typography>
+                </Grid>
+                <Grid xs={1}>
+                  <Icon icon='mdi:timer-sand' color='#32487A' fontSize={'20px'} />
+                </Grid>
+                <Grid xs={11} maxWidth={'90%'}>
+                  <Typography sx={{ color: 'text.primary' }} ml='0.5rem' mt='0.2rem' fontSize={12}>
+                    {item?.contract_duration ? `${item?.contract_duration} months` : '-'}
+                  </Typography>
+                </Grid>
+                {!item?.hide_salary && (
+                  <>
+                    <Grid xs={1}>
+                      <Icon icon='clarity:dollar-line' color='#32487A' fontSize={'20px'} />
+                    </Grid>
+                    <Grid xs={11} maxWidth={'90%'}>
+                      <Typography sx={{ color: 'text.primary' }} ml='0.5rem' mt='0.2rem' fontSize={12}>
+                        {item?.salary_start && item?.salary_end
+                          ? `${item?.salary_start + ' - ' + item?.salary_end} (${item?.currency})`
+                          : '-'}
+                      </Typography>
+                    </Grid>
+                  </>
+                )}
               </>
             ) : (
               <>
@@ -103,6 +149,28 @@ const renderList = (listJob: Job[]) => {
                 <Grid xs={11}>
                   <Typography sx={{ color: 'text.primary' }} ml='0.5rem' mt='0.2rem' fontSize={12}>
                     {item?.degree?.name}
+                  </Typography>
+                </Grid>
+                {!item?.hide_salary && (
+                  <>
+                    <Grid xs={1}>
+                      <Icon icon='clarity:dollar-line' color='#32487A' fontSize={'20px'} />
+                    </Grid>
+                    <Grid xs={11} maxWidth={'90%'}>
+                      <Typography sx={{ color: 'text.primary' }} ml='0.5rem' mt='0.2rem' fontSize={12}>
+                        {item?.salary_start && item?.salary_end
+                          ? `${item?.salary_start + ' - ' + item?.salary_end} (${item?.currency})`
+                          : '-'}
+                      </Typography>
+                    </Grid>
+                  </>
+                )}
+                <Grid xs={1}>
+                  <Icon icon='mdi:location' color='#32487A' fontSize={'20px'} />
+                </Grid>
+                <Grid xs={11}>
+                  <Typography sx={{ color: 'text.primary' }} ml='0.5rem' mt='0.2rem' fontSize={12}>
+                    {item?.city?.city_name} | {item?.employment_type ? item?.employment_type : ''}
                   </Typography>
                 </Grid>
               </>
@@ -124,4 +192,4 @@ const RecomendedView = (props: Props) => {
   )
 }
 
-export default RecomendedView;
+export default RecomendedView
