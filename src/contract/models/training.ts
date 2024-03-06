2b66bedb8c02f8d065b8c61db8ddf190b96b5360
Mikhail <mikhailyuwankho@gmail.com>
2024-03-06 15:54:33 +0700

    add input in manage training

--- src/contract/models/training.ts
+++ src/contract/models/training.ts
@@ -9,6 +9,7 @@ interface Training {
     title: string,
     schedule: string,
     short_description: string,
+    requirement: string,
     joined_at?: string,
     thumbnail: string,
     score: number,
