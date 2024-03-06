53dc63eedf559c37ed7bcd8b873d5b812d453b96
ariesdimasy <ariesdimasy@gmail.com>
2024-03-06 15:45:28 +0700

    fix dob

--- src/layouts/components/CompanyProfile.tsx
+++ src/layouts/components/CompanyProfile.tsx
@@ -226,7 +226,7 @@ const CompanyProfile = (props: compProps) => {
       address_country_id: idcombocode,
       address_city_id: idcity,
       address_address: address,
-      data_of_birth:null
+      date_of_birth:null
     }
     HttpClient.patch(AppConfig.baseUrl + '/user/update-profile', json).then(
       ({ data }) => {
