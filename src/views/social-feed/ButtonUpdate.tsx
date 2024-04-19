c51afc9195792e102d3ff8f6764fb3225b2a3752
Mikhail <mikhailyuwankho@gmail.com>
2024-04-19 10:43:36 +0700

    add delete reply and change button in social feed

--- src/views/social-feed/ButtonUpdate.tsx
+++ src/views/social-feed/ButtonUpdate.tsx
@@ -1,28 +1,24 @@
- 
-import { Button } from "@mui/material";
+import { Button } from '@mui/material'
 import { Icon } from '@iconify/react'
- 
+
 type ButtonLikeParam = {
   id: number
   liked_at?: string
   count_likes: number
 }
 
-  
-
 const ButtonUpdate = (props: { item: ButtonLikeParam; variant?: 'no-icon'; onClick: () => void }) => {
-
-    return (
-      <Button
-        sx={{ fontSize: '0.7rem', textTransform: 'none' }}
-        size='small'
-        color='primary'
-        startIcon={<Icon icon='typcn:edit' fontSize={10} />}
-        onClick={props.onClick}
-      >
-        Update
-      </Button>
-    )
+  return (
+    <Button
+      sx={{ fontSize: '0.7rem', textTransform: 'none' }}
+      size='small'
+      color='primary'
+      startIcon={<Icon icon='typcn:edit' fontSize={10} />}
+      onClick={props.onClick}
+    >
+      Edit
+    </Button>
+  )
 }
 
-export default ButtonUpdate;
+export default ButtonUpdate
