bde4192738e224c20d1d3a0584cb76bf6301a407
Mikhail <mikhailyuwankho@gmail.com>
2024-08-23 00:39:48 +0700

    filter params

--- src/pages/company/find-candidate/index.tsx
+++ src/pages/company/find-candidate/index.tsx
@@ -20,18 +20,18 @@ import {
   Pagination
 } from '@mui/material'
 import CandidateContext, { CandidateProvider } from 'src/context/CandidateContext'
-import { useCandidate } from 'src/hooks/useCandidate'
+import { useRouter } from 'next/router'
+import { useSearchParams } from 'next/navigation'
 import { HttpClient } from 'src/services'
-import { useTheme } from '@mui/material/styles'
-import { Icon } from '@iconify/react'
-import { MdNavigateNext } from 'react-icons/md'
+import { useCandidate } from 'src/hooks/useCandidate'
 import JobCategory from 'src/contract/models/job_category'
 import RoleType from 'src/contract/models/role_type'
 import VesselType from 'src/contract/models/vessel_type'
 import Countries from 'src/contract/models/country'
-import InfiniteScroll from 'react-infinite-scroll-component'
+import { useTheme } from '@mui/material/styles'
+import { Icon } from '@iconify/react'
+import { MdNavigateNext } from 'react-icons/md'
 import RecomendedView from 'src/views/find-candidate/RecomendedView'
-import debounce from 'src/utils/debounce'
 
 const FindCandidate = () => {
   return (
@@ -41,23 +41,50 @@ const FindCandidate = () => {
   )
 }
 
+const pageItem = 15
+
 const FindCandidateApp = () => {
-  const { fetchCandidates, hasNextPage, totalCandidate, page, setPage } = useCandidate()
   const theme = useTheme()
   const hidden = useMediaQuery(theme.breakpoints.down('md'))
   const [collapsed, setCollapsed] = useState<boolean>(true)
-  const [tabValue, setTabValue] = useState<string>('onship')
 
-  const [searchCandidate, SetSearchCandidate] = useState<any>('')
-  const [JobCategory, getJobCategory] = useState<any[]>([])
-  const [combocountry, getComboCountry] = useState<any>([])
-  const [RoleType, getRoleType] = useState<any[]>([])
-  const [VesselType, getVesselType] = useState<any[]>([])
+  const { fetchCandidates, hasNextPage, totalCandidate, page, setPage } = useCandidate()
+  const router = useRouter()
+  const params = useSearchParams()
 
+  const search = params.get('search')
+  const country = params.get('country')
+  const jobCategory = params.get('job_category')
+  const roleType = params.get('role_type')
+  const typeOfVessel = params.get('tov')
+
+  const tabs = params.get('tabs')
+  const [tabValue, setTabValue] = useState<string>(tabs || 'onship')
+  const [JobCategory, getJobCategory] = useState<JobCategory[]>([])
+  const [combocountry, getComboCountry] = useState<Countries[]>([])
+  const [RoleType, getRoleType] = useState<RoleType[]>([])
+  const [VesselType, getVesselType] = useState<VesselType[]>([])
+
+  const [searchCandidate, SetSearchCandidate] = useState<any>(search || '')
+  const [sCountry, setCountry] = useState<any>(null)
   const [sJobCategory, setJobCategory] = useState<any>(null)
   const [sRoleType, setRoleType] = useState<any>(null)
   const [sVesselType, setVesselType] = useState<any>(null)
-  const [country, setCountry] = useState<any>(null)
+
+  useEffect(() => {
+    if (country) {
+      setCountry(combocountry.filter(filter => filter.id == Number(country)))
+    }
+    if (jobCategory) {
+      setJobCategory(JobCategory.filter(filter => filter.id == Number(jobCategory)))
+    }
+    if (RoleType) {
+      setRoleType(RoleType.filter(filter => filter.id == Number(roleType)))
+    }
+    if (typeOfVessel) {
+      setVesselType(VesselType.filter(filter => filter.id == Number(typeOfVessel)))
+    }
+  }, [])
 
   const clearFilter = () => {
     setPage(1)
@@ -69,37 +96,72 @@ const FindCandidateApp = () => {
     setCountry(null)
   }
 
+  const updateParamsFilter = useCallback(() => {
+    const searchParams = new URLSearchParams(params.toString())
+    searchParams.set('tabs', tabValue)
+
+    if (searchCandidate) searchParams.set('search', searchCandidate)
+    else searchParams.delete('search')
+
+    if (sCountry) searchParams.set('country', sCountry.id)
+    else searchParams.delete('country')
+
+    if (sJobCategory) searchParams.set('job_category', sJobCategory.id)
+    else searchParams.delete('job_category')
+
+    if (sRoleType) searchParams.set('role_type', sRoleType.id)
+    else searchParams.delete('role_type')
+
+    if (sVesselType) searchParams.set('tov', sVesselType.id)
+    else searchParams.delete('tov')
+
+    router.replace(
+      {
+        pathname: router.pathname,
+        query: searchParams.toString()
+      },
+      undefined,
+      { shallow: true }
+    )
+  }, [searchCandidate, sCountry, sJobCategory, sRoleType, sVesselType, tabValue, params])
+
+  const handleChangeTabValue = (e: any, value: any) => {
+    setPage(1)
+    setTabValue(value)
+  }
+
   const getListCandidates = async () => {
-    const res2 = await HttpClient.get(`/job-category?search=&page=1&take=250`)
-    if (res2.status != 200) {
-      throw res2.data.message ?? 'Something went wrong!'
+    try {
+      await HttpClient.get('/public/data/country').then(response => {
+        const data = response.data.countries
+        getComboCountry(data)
+      })
+      await HttpClient.get('/job-category?page=1&take=1000').then(response => {
+        const data: JobCategory[] = response.data.categories.data
+        getJobCategory(data.filter(d => d.employee_type == tabValue))
+      })
+      await HttpClient.get('/public/data/vessel-type?page=1&take=1000').then(response => {
+        const data = response.data.vesselTypes.data
+        getVesselType(data)
+      })
+    } catch (error) {
+      throw 'Error fetching filter data'
     }
-    const rawData: JobCategory[] = res2?.data?.categories?.data
-    const filterOnshipCategories = rawData.filter(d => d.employee_type == tabValue)
-    getJobCategory(filterOnshipCategories)
-
-    const res3 = await HttpClient.get(`/public/data/vessel-type?search=&page=1&take=250`)
-    if (res3.status != 200) {
-      throw res3.data.message ?? 'Something went wrong!'
-    }
-    getVesselType(res3.data.vesselTypes.data)
-
-    HttpClient.get('/public/data/country?search=').then(response => {
-      const code = response.data.countries
-      getComboCountry(code)
-    })
   }
 
   const getRoleTypes = async () => {
-    const res4 = await HttpClient.get(`/public/data/role-type`, {
-      page: 1,
-      take: 250,
-      category_id: sJobCategory?.id
-    })
-    if (res4.status != 200) {
-      throw res4.data.message ?? 'Something went wrong!'
+    try {
+      await HttpClient.get('/public/data/role-type', {
+        page: 1,
+        take: 250,
+        category_id: sJobCategory?.id
+      }).then(response => {
+        const data = response.data.roleTypes.data
+        getRoleType(data)
+      })
+    } catch (error) {
+      throw 'Error fetching filter data'
     }
-    getRoleType(res4.data.roleTypes.data)
   }
 
   useEffect(() => {
@@ -111,35 +173,23 @@ const FindCandidateApp = () => {
     getRoleTypes()
   }, [sJobCategory])
 
-  const getdatapencarian = async () => {
-    const objFilter: any = {
-      take: 15,
+  useEffect(() => {
+    fetchCandidates({
+      take: pageItem,
       employee_type: tabValue,
       search: searchCandidate,
       vesseltype_id: sVesselType?.id,
       roletype_id: sRoleType?.id,
       category_id: sJobCategory?.id,
-      country: country?.id
-    }
-    fetchCandidates(objFilter)
-  }
+      country: sCountry?.id
+    })
+    updateParamsFilter()
+  }, [searchCandidate, sVesselType, sRoleType, sJobCategory, tabValue, sCountry])
 
-  useEffect(() => {
-    getdatapencarian()
-  }, [searchCandidate, sVesselType, sRoleType, sJobCategory, tabValue, country])
-
-  const handleChangeTabValue = (e: any, value: any) => {
+  const handleSearch = (value: string) => {
     setPage(1)
-    setTabValue(value)
+    SetSearchCandidate(value)
   }
-
-  const handleSearch = useCallback(
-    debounce((value: string) => {
-      setPage(1)
-      SetSearchCandidate(value)
-    }, 500),
-    []
-  )
 
   return (
     <Grid container spacing={6} justifyContent={'center'}>
@@ -217,6 +267,7 @@ const FindCandidateApp = () => {
                   <TextField
                     fullWidth
                     id='fullName'
+                    defaultValue={searchCandidate}
                     placeholder='Search Candidate Name'
                     variant='outlined'
                     onChange={e => handleSearch(e.target.value)}
@@ -229,7 +280,7 @@ const FindCandidateApp = () => {
                     disablePortal
                     id='combo-box-demo'
                     options={combocountry}
-                    value={country}
+                    value={sCountry}
                     getOptionLabel={(option: any) => option.nicename}
                     renderInput={params => <TextField {...params} placeholder='Choose Country' />}
                     onChange={(event: any, newValue: Countries | null) => {
@@ -357,31 +408,33 @@ const FindCandidateApp = () => {
               }
 
               return (
-                <InfiniteScroll
-                  dataLength={totalCandidate}
-                  next={() => getdatapencarian()}
-                  hasMore={hasNextPage}
-                  loader={
-                    <Typography mt={5} color={'text.secondary'}>
-                      Loading..
-                    </Typography>
-                  }
-                >
-                  <RecomendedView listCandidate={listCandidates} />
-                  <Grid container>
-                    <Typography>{`showing ${
-                      page * 15 < totalCandidate ? page * 15 : totalCandidate
-                    } out of ${totalCandidate} results`}</Typography>
+                <Grid container gap='24px'>
+                  <Grid container sx={{ display: 'flex', justifyContent: 'flex-end', mt: '12px', mb: '-36px' }}>
+                    <Pagination
+                      count={Math.ceil(totalCandidate / pageItem)}
+                      onChange={(e: React.ChangeEvent<unknown>, value: number) => {
+                        setPage(value)
+                      }}
+                      variant='outlined'
+                      shape='rounded'
+                    />
                   </Grid>
-                  <Pagination
-                    count={Math.ceil(totalCandidate / 15)}
-                    onChange={(e: React.ChangeEvent<unknown>, value: number) => {
-                      setPage(value)
-                    }}
-                    variant='outlined'
-                    shape='rounded'
-                  />
-                </InfiniteScroll>
+                  <RecomendedView listCandidate={listCandidates} />
+                  <Grid container sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    <Typography sx={{ color: '#949EA2', fontSize: 14 }}>{`Showing ${
+                      page * pageItem < totalCandidate ? page * pageItem : totalCandidate
+                    } out of ${totalCandidate} results`}</Typography>
+                    <Pagination
+                      sx={{ margin: '0 auto' }}
+                      count={Math.ceil(totalCandidate / pageItem)}
+                      onChange={(e: React.ChangeEvent<unknown>, value: number) => {
+                        setPage(value)
+                      }}
+                      variant='outlined'
+                      shape='rounded'
+                    />
+                  </Grid>
+                </Grid>
               )
             }}
           </CandidateContext.Consumer>
