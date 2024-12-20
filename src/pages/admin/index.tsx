77a2fbf58ac9ece95dced359b853c32e585a6f4f
Mikhail <mikhailyuwankho@gmail.com>
2024-12-20 15:12:10 +0700

    fix ads height and margin issue in admin

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -9,35 +9,33 @@ import CardCompletionPercetage from 'src/views/admin/dashboard/CardCompletionPer
 
 const Home = () => {
   return (
-    <Grid container spacing={6} className='match-height'>
-      <Grid container spacing={6} item xs={12} sm={12} md={12}>
-        <Grid item xs={12} sm={7} md={7}>
+    <Grid container className='match-height' sx={{ display: 'flex', flexDirection: 'column', gap: '24px' }}>
+      <Grid item container spacing={6}>
+        <Grid item xs={12} md={7}>
           <CardUserStat />
         </Grid>
-        <Grid item xs={12} sm={5} md={5}>
+        <Grid item xs={12} md={5}>
           <CardCompletionPercetage />
         </Grid>
       </Grid>
 
-      <Grid item xs={12} sm={6} md={4}>
-        <TopPostCompany />
-      </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-        <TopLastJob />
-      </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-        <TopCategory />
+      <Grid item container spacing={6}>
+        <Grid item xs={12} sm={6} md={4}>
+          <TopPostCompany />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <TopLastJob />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <TopCategory />
+        </Grid>
       </Grid>
 
-      <Grid container spacing={6} item xs={12} sm={12} md={12}>
-        <Grid item xs={12} sm={7} md={7}>
+      <Grid item container spacing={6} sx={{ mt: 12 }}>
+        <Grid item xs={12} md={7}>
           <CardActiveAds />
         </Grid>
-        <Grid item xs={12} sm={5} md={5}>
-        
-        </Grid>
       </Grid>
-
     </Grid>
   )
 }
