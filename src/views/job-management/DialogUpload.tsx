5644c2faf45fd43653b6074af827769e16627449
Mikhail <mikhailyuwankho@gmail.com>
2025-02-03 17:52:47 +0700

    fix company verified first

---
+++ src/views/job-management/DialogUpload.tsx
@@ -0,0 +1,48 @@
+import { Icon } from '@iconify/react'
+import { Box, Dialog, DialogContent, Fade, FadeProps, IconButton, Typography } from '@mui/material'
+import { forwardRef, ReactElement, Ref } from 'react'
+import CompanyDocument from 'src/layouts/components/onboarding/CompanyDocument'
+
+type DialogProps = {
+  visible: boolean
+  onCloseClick: VoidFunction
+}
+
+const Transition = forwardRef(function Transition(
+  props: FadeProps & { children?: ReactElement<any, any> },
+  ref: Ref<unknown>
+) {
+  return <Fade ref={ref} {...props} />
+})
+
+const DialogUpload = (props: DialogProps) => {
+  const { visible, onCloseClick } = props
+
+  return (
+    <Dialog maxWidth='md' fullWidth={true} open={visible} onClose={onCloseClick}>
+      <DialogContent>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Box>
+            <Typography color={'#32487A'} fontWeight='700' fontSize={18}>
+              Add Document
+            </Typography>
+          </Box>
+          <IconButton size='small' onClick={() => onCloseClick()}>
+            <Icon icon='mdi:close' fontSize={'16px'} />
+          </IconButton>
+        </Box>
+        <Box sx={{ mt: '24px' }}>
+          <CompanyDocument
+            beforeLink='none'
+            isEditCompany={true}
+            onClose={() => {
+              onCloseClick()
+            }}
+          />
+        </Box>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export default DialogUpload
