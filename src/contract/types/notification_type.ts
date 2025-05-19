350390f46db7c98c3f114dfafbeedfcbc3f812cf
sudaishakim <sudaishakim@gmail.com>
2025-05-19 10:45:18 +0700

    add job offer notif

--- src/contract/types/notification_type.ts
+++ src/contract/types/notification_type.ts
@@ -1,16 +1,17 @@
 enum NotificationType {
-    feedLike = "App\\Notifications\\FeedLikedNotification",
-    connectRequest = "App\\Notifications\\ConnectRequestNotification",
-    applicantApplied = "App\\Notifications\\ApplicantApplied",
-    newApplicant = "App\\Notifications\\NewApplicantNotification",
-    connectRequestApproved = "App\\Notifications\\ConnectRequestApproved",
-    connectRequestRejected = "App\\Notifications\\ConnectRequestRejected",
-    completeProfileEncouragement = "App\\Notifications\\CompleteProfileEncouragement",
-    companyApproval = "App\\Notifications\\NotificationCompanyApproval",
-    companyOnboarding = "App\\Notifications\\NotificationCompanyOnboarding",
-    applicantViewed = "App\\Notifications\\ApplicantViewed",
-    applicantRejected = "App\\Notifications\\ApplicantRejected",
-    applicantApproved = "App\\Notifications\\ApplicantApproved",
+  feedLike = 'App\\Notifications\\FeedLikedNotification',
+  connectRequest = 'App\\Notifications\\ConnectRequestNotification',
+  applicantApplied = 'App\\Notifications\\ApplicantApplied',
+  newApplicant = 'App\\Notifications\\NewApplicantNotification',
+  connectRequestApproved = 'App\\Notifications\\ConnectRequestApproved',
+  connectRequestRejected = 'App\\Notifications\\ConnectRequestRejected',
+  completeProfileEncouragement = 'App\\Notifications\\CompleteProfileEncouragement',
+  companyApproval = 'App\\Notifications\\NotificationCompanyApproval',
+  companyOnboarding = 'App\\Notifications\\NotificationCompanyOnboarding',
+  applicantViewed = 'App\\Notifications\\ApplicantViewed',
+  applicantRejected = 'App\\Notifications\\ApplicantRejected',
+  applicantApproved = 'App\\Notifications\\ApplicantApproved',
+  jobOffer = 'App\\Notifications\\JobOfferNotification'
 }
 
-export default NotificationType;
+export default NotificationType
