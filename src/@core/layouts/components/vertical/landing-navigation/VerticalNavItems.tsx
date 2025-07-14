7706acb43f2668906043d7c57eb337fc438b964c
Dais <dais@Daiss-MacBook-Air.local>
2025-07-14 13:14:02 +0700

    fix onclose after click menu mobile

--- src/@core/layouts/components/vertical/landing-navigation/VerticalNavItems.tsx
+++ src/@core/layouts/components/vertical/landing-navigation/VerticalNavItems.tsx
@@ -2,44 +2,67 @@ import { NavLink, NavGroup, LayoutProps } from 'src/@core/layouts/types'
 import VerticalNavLink from './VerticalNavLink'
 import NavItemType from 'src/contract/types/navItemType'
 
+import { useMediaQuery, useTheme } from '@mui/material'
+
 interface Props {
-  parent?: NavGroup,
-  navHover?: boolean,
-  navVisible?: boolean,
-  groupActive: string[],
-  isSubToSub?: NavGroup,
-  currentActiveGroup: string[],
-  navigationBorderWidth: number,
-  settings: LayoutProps['settings'],
-  saveSettings: LayoutProps['saveSettings'],
-  setGroupActive: (value: string[]) => void,
-  setCurrentActiveGroup: (item: string[]) => void,
-  homeNavItems: { title: string, path: string }[],
-  navItems: NavItemType[],
+  parent?: NavGroup
+  navHover?: boolean
+  navVisible?: boolean
+  groupActive: string[]
+  isSubToSub?: NavGroup
+  currentActiveGroup: string[]
+  navigationBorderWidth: number
+  settings: LayoutProps['settings']
+  saveSettings: LayoutProps['saveSettings']
+  setGroupActive: (value: string[]) => void
+  setCurrentActiveGroup: (item: string[]) => void
+  homeNavItems: { title: string; path: string }[]
+  navItems: NavItemType[]
+  setNavVisible?: (value: boolean) => void
 }
 
 const VerticalNavItems = (props: Props) => {
-  const { homeNavItems, navItems } = props;
-  const menus: { title: string, path: string }[] = [];
+  const isMobileMd = useMediaQuery(useTheme().breakpoints.down('md'))
+  const { homeNavItems, navItems } = props
+  const menus: { title: string; path: string }[] = []
 
-  homeNavItems.forEach((item) => {
-    menus.push(item);
-  });
+  homeNavItems.forEach(item => {
+    menus.push(item)
+  })
 
-  navItems.forEach((item) => {
+  navItems.forEach(item => {
     menus.push({
       title: item.title,
-      path: item.onClick,
-    });
-  });
+      path: item.onClick
+    })
+  })
 
   const RenderMenuItems = menus.map((item, index) => {
     const menuItem: NavLink = {
       title: item.title,
-      path: item.path,
-    };
+      path: item.path
+    }
 
-    return <VerticalNavLink settings={props.settings} navigationBorderWidth={1} key={index} item={menuItem} collapsedNavWidth={1} toggleNavVisibility={() => { return; }} />
+    if ((isMobileMd && item.path === '/register') || item.path === '/login') {
+      return null
+    }
+
+    return (
+      <VerticalNavLink
+        settings={props.settings}
+        navigationBorderWidth={1}
+        key={index}
+        item={menuItem}
+        collapsedNavWidth={1}
+        toggleNavVisibility={() => {
+          if (props.setNavVisible) {
+            props?.setNavVisible(false)
+          }
+
+          return
+        }}
+      />
+    )
   })
 
   return <>{RenderMenuItems}</>
